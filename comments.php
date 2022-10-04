d5351833d300a42af1c50b2b55d093a82f9f9552
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-04 17:43:55 +0545

    Add basic footer

---
+++ comments.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying comments
+ *
+ * This is the template that displays the area of the page that contains both the current comments
+ * and the comment form.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ * @since x.x.x
+ * @package Theme
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( post_password_required() ) {
+	return;
+}
+?>
+<div id="comments" class="comments-area">
+<?php
+	do_action( 'theme_before_comments' );
+	do_action( 'theme_comments' );
+	do_action( 'theme_after_comments' );
+?>
+</div><!-- #comments -->
