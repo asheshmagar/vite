db065116477ffd543efe1ee4dac210449283d93a
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-04 17:42:18 +0545

    Add action for no sidebar

--- sidebar.php
+++ sidebar.php
@@ -11,12 +11,18 @@ namespace Theme;
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
+
+$sidebar = apply_filters( 'theme_sidebar', 'sidebar-1' );
 ?>
 
 <aside id="secondary">
 	<?php
-		do_action( 'theme_before_sidebar' );
-		do_action( 'theme_sidebar' );
-		do_action( 'theme_after_sidebar' );
+	do_action( 'theme_before_sidebar' );
+	if ( is_active_sidebar( $sidebar ) ) {
+		dynamic_sidebar( $sidebar );
+	} else {
+		do_action( 'theme_no_sidebar' );
+	}
+	do_action( 'theme_after_sidebar' );
 	?>
 </aside>
