76a2b81b9aeee5c9054882f1bc6188d21a2cc548
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-04 17:43:10 +0545

    Add markup and fix html lang attribute

--- header.php
+++ header.php
@@ -14,7 +14,7 @@ namespace Theme;
 defined( 'ABSPATH' ) || exit;
 ?>
 <!doctype html>
-<html class="no-js" lang="<?php language_attributes(); ?>">
+<html class="no-js" <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
@@ -22,10 +22,13 @@ defined( 'ABSPATH' ) || exit;
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
-		<?php
-		wp_body_open();
-
-		do_action( 'theme_before_header' );
-		do_action( 'theme_header' );
-		do_action( 'theme_after_header' );
-		?>
+		<?php wp_body_open(); ?>
+		<div id="page" class="site wp-site-blocks">
+			<?php
+			do_action( 'theme_before_header' );
+			do_action( 'theme_header' );
+			do_action( 'theme_after_header' );
+			?>
+			<div id="content" class="site-content">
+				<div class="container">
+				<?php do_action( 'theme_before_content' ); ?>
