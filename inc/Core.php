02904ae54178d049b5db43e427f656da00fda61c
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-04 17:45:41 +0545

    Add Core service provider

---
+++ inc/Core.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * The loop.
+ *
+ * @since 1.0.0
+ * @package Theme
+ */
+
+namespace Theme;
+
+/**
+ * Core.
+ */
+class Core {
+
+	/**
+	 * The loop.
+	 *
+	 * @return void
+	 */
+	public function the_loop() {
+		do_action( 'theme_before_the_loop' );
+		if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post();
+				do_action( 'theme_the_loop' );
+			}
+		} else {
+			do_action( 'theme_no_posts' );
+		}
+		do_action( 'theme_after_the_loop' );
+	}
+
+	/**
+	 * Sidebar title.
+	 *
+	 * @param string $id Sidebar id.
+	 * @return string|void
+	 */
+	public function render_sidebar_title( string $id ) {
+		global $wp_registered_sidebars;
+
+		if ( ! isset( $id ) || ! isset( $wp_registered_sidebars[ $id ]['name'] ) ) {
+			return '';
+		}
+
+		echo esc_html( $wp_registered_sidebars[ $id ]['name'] );
+	}
+
+	/**
+	 * Get the current post ID.
+	 *
+	 * @return int
+	 */
+	public function get_the_id(): int {
+		if ( is_home() && 'page' === get_option( 'show_on_front' ) ) {
+			$post_id = (int) get_option( 'page_for_posts' );
+		} elseif ( is_front_page() && 'page' === get_option( 'show_on_front' ) ) {
+			$post_id = (int) get_option( 'page_on_front' );
+		} elseif ( is_singular() ) {
+			$post_id = (int) get_the_ID();
+		} elseif ( is_category() || is_tag() || is_tax() ) {
+			$post_id = get_queried_object_id();
+		} else {
+			$post_id = 0;
+		}
+
+		return $post_id;
+	}
+
+	/**
+	 * Static theme string.
+	 *
+	 * @param string $id String id.
+	 * @param bool   $should_return Whether to return or echo string.
+	 * @return string|void
+	 */
+	public function static_strings( string $id, bool $should_return = false ) {
+		$strings = [
+			'no-posts'        => __( 'No posts found.', 'theme' ),
+			'skip-to-content' => __( 'Skip to content', 'theme' ),
+			'go-to-top'       => __( 'Go to top', 'theme' ),
+			'leave-a-comment' => __( 'Leave a comment', 'theme' ),
+			'primary-menu'    => __( 'Primary Menu', 'theme' ),
+			'secondary-menu'  => __( 'Secondary Menu', 'theme' ),
+			'footer-menu'     => __( 'Footer Menu', 'theme' ),
+
+		];
+
+		if ( ! isset( $id ) || ! isset( $strings[ $id ] ) ) {
+			return '';
+		}
+
+		if ( $should_return ) {
+			return $strings[ $id ];
+		}
+
+		echo esc_html( $strings[ $id ] );
+	}
+
+	/**
+	 * Get default options.
+	 *
+	 * @param string $id Id.
+	 * @return string|string[]
+	 */
+	public function get_default_options( string $id ) {
+
+		$defaults = [
+			'post_page_header' => 'style-1',
+			'page_page_header' => 'style-1',
+		];
+
+		if ( ! isset( $id ) || ! isset( $defaults[ $id ] ) ) {
+			return $defaults;
+		}
+
+		return $defaults[ $id ];
+	}
+}
