1b2f72c164ed2d1f24acdb1dbace1c11b976b380
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-05 23:05:04 +0545

    Change slugs

--- functions.php
+++ functions.php
@@ -12,9 +12,9 @@ defined( 'ABSPATH' ) || exit;
 /**
  * Define constants.
  */
-! defined( 'THEME_VERSION' ) && define( 'THEME_VERSION', 'x.x.x' );
-! defined( 'THEME_ASSETS_DIR' ) && define( 'THEME_ASSETS_DIR', get_theme_file_path( '/assets/' ) );
-! defined( 'THEME_ASSETS_URI' ) && define( 'THEME_ASSETS_URI', get_theme_file_uri( '/assets/' ) );
+! defined( 'VITE_VERSION' ) && define( 'VITE_VERSION', 'x.x.x' );
+! defined( 'VITE_ASSETS_DIR' ) && define( 'VITE_ASSETS_DIR', get_theme_file_path( '/assets/' ) );
+! defined( 'VITE_ASSETS_URI' ) && define( 'VITE_ASSETS_URI', get_theme_file_uri( '/assets/' ) );
 
 // Load composer autoloader.
 require_once __DIR__ . '/vendor/autoload.php';
