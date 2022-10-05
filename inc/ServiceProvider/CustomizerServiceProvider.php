daf49337c0d3d4016cd45fc0fad0aa29e4923759
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-05 23:08:24 +0545

    Add service provider for customizer

---
+++ inc/ServiceProvider/CustomizerServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Core service provider.
+ *
+ * @package Vite
+ * @since 1.0.0
+ */
+
+namespace Vite\ServiceProvider;
+
+defined( 'ABSPATH' ) || exit;
+
+use League\Container\ServiceProvider\AbstractServiceProvider;
+use Vite\Customizer\Customizer;
+
+/**
+ * Core service provider.
+ */
+class CustomizerServiceProvider extends AbstractServiceProvider {
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @param string $id Service id.
+	 */
+	public function provides( string $id ): bool {
+		$services = [
+			'customizer',
+			Customizer::class,
+		];
+
+		return in_array( $id, $services, true );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function register(): void {
+		$this->getContainer()->addShared( 'customizer', Customizer::class );
+	}
+}
