8a27c92951677b87575a8cb3e6fa6150a1fad8db
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-08 23:12:40 +0545

    Export components

--- assets/js/customizer/components/index.js
+++ assets/js/customizer/components/index.js
@@ -2,3 +2,5 @@ export { default as Tooltip } from './tooltip';
 export { default as CustomindColorPicker } from './color-picker';
 export { default as CustomindRange } from './range';
 export { default as UnitPicker } from './unit';
+export { default as Popover } from './popover';
+export { default as Reset } from './reset';
