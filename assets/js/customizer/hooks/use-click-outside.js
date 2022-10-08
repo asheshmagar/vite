7af9c14728544e148b574b66cd6ca526c1435648
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-08 23:12:59 +0545

    Refactor use click outside hook

--- assets/js/customizer/hooks/use-click-outside.js
+++ assets/js/customizer/hooks/use-click-outside.js
@@ -1,17 +1,29 @@
-import { useEffect } from '@wordpress/element';
+import { useEffect, useMemo, useRef } from '@wordpress/element';
 
-export default ( trigger, popover, callback ) => {
+export default ( el, isOpen ) => {
+	const ref = useRef();
+
 	useEffect( () => {
-		const wrapper = document.querySelector( '.wp-full-overlay-sidebar-content' );
-		if ( ! popover.current || ! trigger.current || ! wrapper ) return;
+		const wrap = document.querySelector( '.wp-full-overlay-sidebar-content' );
+		if ( ! ref.current || ! el.current || ! wrap ) return;
 		const listener = ( e ) => {
-			if ( ! popover.current?.contains( e.target ) && ! trigger.current?.contains( e.target ) ) {
-				callback();
+			if ( ! ref.current?.contains( e.target ) && ! el.current?.contains( e.target ) ) {
+				if ( isOpen ) {
+					el.current?.click();
+				}
 			}
 		};
-		wrapper.addEventListener( 'click', listener );
+		wrap.addEventListener( 'click', listener );
 		return () => {
-			wrapper.removeEventListener( 'click', listener );
+			wrap.removeEventListener( 'click', listener );
 		};
-	}, [] );
+	}, [ isOpen ] );
+
+	return useMemo( () => {
+		return ( { children, ...props } ) => (
+			<div className="vite-popover" ref={ ref } style={ { display: isOpen ? undefined : 'none' } } { ...props }>
+				{ children }
+			</div>
+		);
+	}, [ isOpen ] );
 };
