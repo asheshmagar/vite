a8dd103ef3b72600a627ee3529567923b34dddeb
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-09 22:46:58 +0545

    Export util function isNumeric

--- assets/js/customizer/utils/index.js
+++ assets/js/customizer/utils/index.js
@@ -1,3 +1,4 @@
 export { default as registerControl } from './register-control';
 export { default as getObjectValues } from './get-object-values';
 export { default as splitUnit } from './split-unit';
+export { default as isNumeric } from './is-numeric';
