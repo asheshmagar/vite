ba78d9713512873ebf3e80ce9ccd045f34faed21
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-11 10:37:09 +0545

    Device selector as dropdown or normal

--- assets/js/customizer/hooks/use-device-selector.js
+++ assets/js/customizer/hooks/use-device-selector.js
@@ -19,36 +19,53 @@ export default () => {
 	}, [] );
 
 	const DeviceSelector = useMemo( () => {
-		return ( props ) => (
+		return ( { dropdown = true, ...props } ) => (
 			<div className="vite-device-selector" { ...props }>
-				<Dropdown
-					className="vite-devices"
-					position="top center"
-					renderToggle={ ( { isOpen, onToggle } ) => (
-						<Button
-							className="vite-device"
-							onClick={ onToggle }
-							aria-expanded={ isOpen }
-							icon={ 'mobile' === device ? 'smartphone' : device }
-						/>
-					) }
-					renderContent={ () => (
-						<ButtonGroup>
-							{ [ 'desktop', 'tablet', 'mobile' ].map( d => (
-								<Button
-									key={ d }
-									className={ `vite-device ${ d }${ device === d ? ' is-primary' : '' }` }
-									onClick={ ( e ) => {
-										e.stopPropagation();
-										setDevice( d );
-										wp?.customize?.previewedDevice?.set( d );
-									} }
-									icon={ 'mobile' === d ? 'smartphone' : d }
-								/>
-							) ) }
-						</ButtonGroup>
-					) }
-				/>
+				{ dropdown ? (
+					<Dropdown
+						className="vite-devices"
+						position="top center"
+						renderToggle={ ( { isOpen, onToggle } ) => (
+							<Button
+								className="vite-device"
+								onClick={ onToggle }
+								aria-expanded={ isOpen }
+								icon={ 'mobile' === device ? 'smartphone' : device }
+							/>
+						) }
+						renderContent={ () => (
+							<ButtonGroup>
+								{ [ 'desktop', 'tablet', 'mobile' ].map( d => (
+									<Button
+										key={ d }
+										className={ `vite-device ${ d }${ device === d ? ' is-primary' : '' }` }
+										onClick={ ( e ) => {
+											e.stopPropagation();
+											setDevice( d );
+											wp?.customize?.previewedDevice?.set( d );
+										} }
+										icon={ 'mobile' === d ? 'smartphone' : d }
+									/>
+								) ) }
+							</ButtonGroup>
+						) }
+					/>
+				) : (
+					<ButtonGroup className="vite-devices">
+						{ [ 'desktop', 'tablet', 'mobile' ].map( d => (
+							<Button
+								key={ d }
+								className={ `vite-device ${ d }${ device === d ? ' is-primary' : '' }` }
+								onClick={ ( e ) => {
+									e.stopPropagation();
+									setDevice( d );
+									wp?.customize?.previewedDevice?.set( d );
+								} }
+								icon={ 'mobile' === d ? 'smartphone' : d }
+							/>
+						) ) }
+					</ButtonGroup>
+				) }
 			</div>
 		);
 	}, [ device ] );
