b613da27f851728c087d67142bc7b4cbf37cd79f
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-11 10:39:13 +0545

    Tweaks

--- inc/Customizer/DynamicCSS.php
+++ inc/Customizer/DynamicCSS.php
@@ -13,6 +13,26 @@ namespace Vite\Customizer;
  */
 class DynamicCSS {
 
+	const STATES = [
+		'normal',
+		'hover',
+		'active',
+		'focus',
+	];
+
+	const DEVICES = [
+		'desktop',
+		'tablet',
+		'mobile',
+	];
+
+	const SIDES = [
+		'top',
+		'right',
+		'bottom',
+		'left',
+	];
+
 	/**
 	 * CSS data.
 	 *
@@ -48,10 +68,10 @@ class DynamicCSS {
 					$css .= $v;
 					break;
 				case 'tablet':
-					$css .= '@media (max-width: 1024px) {' . $v . '}';
+					$css .= sprintf( '@media (max-width: 1024px) { %s }', $v );
 					break;
 				case 'mobile':
-					$css .= '@media (max-width: 767px) {' . $v . '}';
+					$css .= sprintf( '@media (max-width: 767px) { %s }', $v );
 					break;
 			}
 		}
@@ -66,19 +86,13 @@ class DynamicCSS {
 	 * @return void
 	 */
 	public function add( array $data = [] ) {
-		if (
-			! isset( $data['selector'] ) ||
-			! isset( $data['properties'] ) ||
-			! isset( $data['name'] )
-		) {
-			return;
-		}
-
 		$this->css_data[] = [
 			'name'       => $data['name'],
 			'control'    => $data['control'] ?? null,
-			'selectors'  => $data['selectors'] ?? null,
-			'properties' => $data['properties'] ?? null,
+			'selectors'  => $data['selectors'] ?? [],
+			'properties' => $data['properties'] ?? [],
+			'context'    => $data['context'] ?? null,
+			'extra'      => $data['input_attrs'] ?? null,
 		];
 	}
 
@@ -90,86 +104,207 @@ class DynamicCSS {
 	public function make(): DynamicCSS {
 		if ( ! empty( $this->css_data ) ) {
 			foreach ( $this->css_data as $d ) {
-				if ( ! isset( $d['selectors'] ) ) {
-					continue;
-				}
-				$type  = $d['control'] ?? null;
-				$value = get_theme_mod( $d['name'] );
+				$type = $d['control'] ?? null;
+
+				preg_match( '/\[([^]]*)]/', $d['name'], $match );
+
+				$value = vite( 'customizer' )->get_setting( $match[1] );
+
 				switch ( $type ) {
 					case 'vite-dimensions':
-						if ( ! empty( $value ) ) {
+						if ( ! empty( $value ) && is_array( $value ) ) {
 							if ( $this->array_some(
 								function( $a ) {
-									return in_array( $a, [ 'top', 'right', 'bottom', 'left' ], true );
+									return in_array( $a, static::SIDES, true );
 								},
-								$value
+								array_keys( $value )
 							) ) {
-								$this->css['desktop'] = $this->make_css( $d['selectors'], $d['properties'], $this->dimension_css( $value ) );
+								$this->css['desktop'] .= $this->make_css( $d['selectors'], $d['properties'], $this->dimension_css( $value ) );
 							} elseif ( $this->array_some(
 								function( $a ) {
-									return in_array( $a, [ 'desktop', 'tablet', 'mobile' ], true );
+									return in_array( $a, static::DEVICES, true );
 								},
-								$value
+								array_keys( $value )
 							) ) {
-								foreach ( $value as $d => $v ) {
-									$this->css[ $d ] = $this->make_css( $d['selectors'], $d['properties'], $this->dimension_css( $v ?? [] ) ?? '' );
-								}
-							}
-						}
-						break;
-					case 'vite-background':
-						if ( ! empty( $value ) ) {
-							$this->css['desktop'] .= $this->make_css( $d['selectors'], $d['properties'], $this->background_css( $value ) );
-						}
-						break;
-					case 'vite-typography':
-						if ( ! empty( $value ) ) {
-							$desktop_size = $value['size']['desktop'] ?? '';
-
-							if ( ! empty( $value['size'] ) ) {
-								foreach ( $value['size']  as $k => $v ) {
-									if ( 'desktop' === $k || empty( $v ) ) {
-										continue;
+								foreach ( static::DEVICES as $device ) {
+									if ( isset( $value[ $device ] ) ) {
+										$this->css[ $device ] .= $this->make_css( $d['selectors'], $d['properties'], $this->dimension_css( $value[ $device ] ) );
 									}
-									$this->css[ $k ] .= $this->make_css( $d['selectors'], [ 'font-size' ], $v );
 								}
-
-								unset( $value['size'] );
 							}
-
-							$css = implode( ',', $d['selectors'] ) . ' {';
-
-							foreach ( $value as $k => $v ) {
-								if ( empty( $v ) ) {
-									continue;
-								}
-								$css .= $k . ':' . $v . ';';
-							}
-
-							if ( ! empty( $desktop_size ) ) {
-								$css .= 'font-size:' . $desktop_size . ';';
-							}
-
-							$css .= '}';
-
-							$this->css['desktop'] .= $css;
 						}
 						break;
-					default:
+					case 'vite-slider':
 						if ( ! empty( $value ) ) {
 							if ( is_array( $value ) ) {
-								foreach ( $value as $k => $v ) {
-									if ( ! empty( $v ) ) {
-										$this->css[ $k ] .= $this->make_css( $d['selectors'], $d['properties'], $v );
+								if ( $this->array_some(
+									function( $a ) {
+										return in_array( $a, static::DEVICES, true );
+									},
+									array_keys( $value )
+								) ) {
+									foreach ( static::DEVICES as $device ) {
+										if ( isset( $value[ $device ] ) ) {
+											$values = $value[ $device ];
+											if ( is_scalar( $values ) ) {
+												$this->css[ $device ] .= $this->make_css( $d['selectors'], $d['properties'], $values );
+											} else {
+												if ( isset( $values['value'] ) ) {
+													$unit                  = $values['unit'] ?? ( $d['extra']['defaultUnit'] ?? 'px' );
+													$unit                  = '-' === $unit ? '' : $unit;
+													$this->css[ $device ] .= $this->make_css( $d['selectors'], $d['properties'], "{$values['value']}$unit" );
+												}
+											}
+										}
+									}
+								} else {
+									if ( isset( $value['value'] ) ) {
+										$unit                  = $value['unit'] ?? ( $d['extra']['defaultUnit'] ?? 'px' );
+										$unit                  = '-' === $unit ? '' : $unit;
+										$this->css['desktop'] .= $this->make_css( $d['selectors'], $d['properties'], "{$value['value']}$unit" );
 									}
 								}
 							} else {
 								$this->css['desktop'] .= $this->make_css( $d['selectors'], $d['properties'], $value );
 							}
 						}
+						break;
+					case 'vite-background':
+						if ( ! empty( $value ) ) {
+							$type = $value['type'] ?? 'color';
+
+							if ( 'gradient' === $type && isset( $value['gradient'] ) ) {
+								$this->css['desktop'] .= $this->make_css( $d['selectors'], [], "background: {$value['gradient']};" );
+							} else {
+								unset( $value['gradient'] );
+								unset( $value['type'] );
+
+								$desktop = [];
+								$tablet  = [];
+								$mobile  = [];
+
+								foreach ( static::DEVICES as $device ) {
+									foreach ( $value  as $k => $v ) {
+										if ( isset( $v ) && is_scalar( $v ) ) {
+											$desktop[ $k ] = $v;
+											continue;
+										}
+
+										if ( isset( $v[ $device ] ) ) {
+											${$device}[ $k ] = $v[ $device ];
+										}
+									}
+								}
+
+								if ( ! empty( $tablet ) ) {
+									$this->css['tablet'] .= $this->make_css( $d['selectors'], $d['properties'], $this->background_css( $tablet ) );
+								}
+								if ( ! empty( $mobile ) ) {
+									$this->css['mobile'] .= $this->make_css( $d['selectors'], $d['properties'], $this->background_css( $mobile ) );
+								}
+								if ( ! empty( $desktop ) ) {
+									$this->css['desktop'] .= $this->make_css( $d['selectors'], $d['properties'], $this->background_css( $desktop ) );
+								}
+							}
+						}
+						break;
+					case 'vite-typography':
+						if ( ! empty( $value ) ) {
+							$desktop = [];
+							$tablet  = [];
+							$mobile  = [];
+
+							foreach ( static::DEVICES as $device ) {
+								foreach ( $value as $k => $v ) {
+									if ( isset( $v ) && is_scalar( $v ) ) {
+										$desktop[ $k ] = $v;
+										continue;
+									}
+									if ( isset( $v[ $device ] ) ) {
+										${$device}[ $k ] = $v[ $device ];
+									}
+								}
+							}
+
+							if ( ! empty( $desktop ) ) {
+								$this->css['desktop'] .= $this->make_css( $d['selectors'], [], $this->typography_css( $desktop ) );
+							}
+
+							if ( ! empty( $tablet ) ) {
+								$this->css['tablet'] .= $this->make_css( $d['selectors'], [], $this->typography_css( $tablet ) );
+							}
+
+							if ( ! empty( $mobile ) ) {
+								$this->css['mobile'] .= $this->make_css( $d['selectors'], [], $this->typography_css( $mobile ) );
+							}
+						}
+						break;
+					case 'vite-border':
+						if ( ! empty( $value ) ) {
+							if ( isset( $value['color']['hover'] ) ) {
+								$this->css['desktop'] .= $this->make_css(
+									array_map(
+										function( $s ) {
+											return $s . ':hover';
+										},
+										$d['selectors']
+									),
+									[ 'border-color' ],
+									$value['color']['hover']
+								);
+							}
+							$this->css['desktop'] .= $this->make_css( $d['selectors'], [], $this->border_css( $value ) );
+						}
+						break;
+					default:
+						if ( ! empty( $value ) ) {
+							if ( is_array( $value ) ) {
+								if ( $this->array_some(
+									function( $a ) {
+										return in_array( $a, static::DEVICES, true );
+									},
+									array_keys( $value )
+								) ) {
+									foreach ( static::DEVICES as $device ) {
+										if ( isset( $value[ $device ] ) ) {
+											$this->css[ $device ] .= $this->make_css( $d['selectors'], $d['properties'], (string) $value[ $device ] );
+										}
+									}
+								}
+
+								if ( $this->array_some(
+									function( $a ) {
+										return in_array( $a, static::STATES, true );
+									},
+									array_keys( $value )
+								) ) {
+									foreach ( $value as $k => $v ) {
+										$this->css['desktop'] .= $this->make_css(
+											array_map(
+												function( $s ) use ( $k ) {
+													if ( empty( $k ) || ! in_array( $k, static::STATES, true ) ) {
+														return false;
+													}
+													if ( 'normal' === $k ) {
+														return $s;
+													}
+													return "$s:$k";
+												},
+												$d['selectors']
+											),
+											$d['properties'],
+											(string) $v
+										);
+									}
+								}
+							} else {
+								$this->css['desktop'] .= $this->make_css( $d['selectors'], $d['properties'], (string) $value );
+							}
+						}
 				}
 			}
 		}
+
 		return $this;
 	}
 
@@ -185,7 +320,13 @@ class DynamicCSS {
 		if ( empty( $value ) ) {
 			return '';
 		}
-		return sprintf( '%s { %s }', implode( ',', $selectors ), implode( ": $value;", $properties ) );
+
+		if ( ! empty( $properties ) ) {
+			$properties[] = '';
+			return sprintf( '%s { %s }', implode( ',', $selectors ), implode( ": $value;", $properties ) );
+		}
+
+		return sprintf( '%s { %s }', implode( ',', $selectors ), $value );
 	}
 
 	/**
@@ -195,10 +336,96 @@ class DynamicCSS {
 	 * @return string
 	 */
 	private function dimension_css( array $value = [] ): string {
-		$css  = $value['top'] ?? '0px';
-		$css .= ' ' . ( $value['right'] ?? '0px' );
-		$css .= ' ' . ( $value['bottom'] ?? '0px' );
-		$css .= ' ' . ( $value['left'] ?? '0px' );
+		$unit = $value['unit'] ?? 'px';
+
+		$css  = ( $value['top'] ?? 0 ) . $unit;
+		$css .= ' ' . ( $value['right'] ?? 0 ) . $unit;
+		$css .= ' ' . ( $value['bottom'] ?? 0 ) . $unit;
+		$css .= ' ' . ( $value['left'] ?? 0 ) . $unit;
+
+		return $css;
+	}
+
+	/**
+	 * Border CSS.
+	 *
+	 * @param array $value Border value.
+	 * @return string
+	 */
+	private function border_css( array $value = [] ): string {
+		$css = '';
+
+		if ( isset( $value['radius'] ) ) {
+			$radius = $value['radius'];
+
+			if ( ! empty( $radius['value'] ) ) {
+				$radius['unit'] = $radius['unit'] ?? 'px';
+				$css           .= "border-radius: {$radius['value']}{$radius['unit']};";
+			}
+		}
+
+		if ( isset( $value['style'] ) && 'none' !== $value['style'] ) {
+			$css .= "border-style: {$value['style']};";
+
+			if ( isset( $value['width'] ) ) {
+				$width = $value['width'];
+
+				if ( ! empty( $width['value'] ) ) {
+					$width['unit'] = $width['unit'] ?? 'px';
+					$css          .= "border-width: {$width['value']}{$width['unit']};";
+				}
+			}
+
+			if ( isset( $value['color']['normal'] ) ) {
+				$css .= "border-color: {$value['color']['normal']};";
+			}
+		}
+
+		return $css;
+	}
+
+	/**
+	 * Typography CSS.
+	 *
+	 * @param array $value Typography value.
+	 * @return string
+	 */
+	private function typography_css( array $value = [] ):string {
+		$css = '';
+		if ( ! empty( $value['family'] ) ) {
+			$css .= "font-family: {$value['family']};";
+		}
+		if ( ! empty( $value['size'] ) ) {
+			$size = $value['size'];
+			if ( ! empty( $size['value'] ) ) {
+				$size['unit'] = $size['unit'] ?? 'px';
+				$css         .= "font-size: {$size['value']}{$size['unit']};";
+			}
+		}
+		if ( ! empty( $value['weight'] ) ) {
+			$css .= "font-weight: {$value['weight']};";
+		}
+		if ( ! empty( $value['style'] ) ) {
+			$css .= "font-style: {$value['style']};";
+		}
+		if ( ! empty( $value['lineHeight'] ) ) {
+			$line_height = $value['lineHeight'];
+			if ( ! empty( $line_height['value'] ) ) {
+				$line_height['unit'] = $line_height['unit'] ?? '';
+				$line_height['unit'] = '-' === $line_height['unit'] ? '' : $line_height['unit'];
+				$css                .= "line-height: {$line_height['value']}{$line_height['unit']};";
+			}
+		}
+		if ( ! empty( $value['letterSpacing'] ) ) {
+			$letter_spacing = $value['letterSpacing'];
+			if ( ! empty( $letter_spacing['value'] ) ) {
+				$letter_spacing['unit'] = $letter_spacing['unit'] ?? 'px';
+				$css                   .= "letter-spacing: {$letter_spacing['value']}{$letter_spacing['unit']};";
+			}
+		}
+		if ( ! empty( $value['transform'] ) ) {
+			$css .= "text-transform: {$value['transform']};";
+		}
 
 		return $css;
 	}
@@ -212,15 +439,26 @@ class DynamicCSS {
 	private function background_css( array $value = [] ): string {
 		$css = '';
 
-		$css .= $value['color'] ?? '';
-
 		if ( ! empty( $value['image'] ) ) {
-			$css  = 'url(' . $value['image'] . ') ';
-			$css .= ( $value['position'] ?? '0% 0%' ) . '/';
-			$css .= $value['size'] ?? 'auto ';
-			$css .= $value['repeat'] ?? 'repeat ';
-			$css .= $value['attachment'] ?? 'scroll ';
+			$css  = 'background: url(' . $value['image'] . ') ';
+			$css .= ( $value['position'] ?? '50% 50%' ) . '/';
+			$css .= ( $value['size'] ?? 'auto' ) . ' ';
+			$css .= ( $value['repeat'] ?? 'repeat' ) . ' ';
+			$css .= ( $value['attachment'] ?? 'scroll' ) . ' ';
 			$css .= $value['color'] ?? '';
+		} else {
+			if ( isset( $value['position'] ) ) {
+				$css .= "background-position: {$value['position']};";
+			}
+			if ( isset( $value['size'] ) ) {
+				$css .= "background-size: {$value['size']};";
+			}
+			if ( isset( $value['repeat'] ) ) {
+				$css .= "background-repeat: {$value['repeat']};";
+			}
+			if ( isset( $value['attachment'] ) ) {
+				$css .= "background-attachment: {$value['attachment']};";
+			}
 		}
 
 		return $css;
@@ -256,6 +494,8 @@ class DynamicCSS {
 			return;
 		}
 
+		$css = $this->minify( $css );
+
 		global $wp_filesystem;
 		$upload_dir_url = wp_upload_dir();
 		$upload_dir     = trailingslashit( $upload_dir_url['basedir'] ) . 'vite/';
@@ -268,5 +508,43 @@ class DynamicCSS {
 		! $wp_filesystem->is_dir( $upload_dir ) && $wp_filesystem->mkdir( $upload_dir );
 
 		$wp_filesystem->put_contents( "$upload_dir$filename", $css );
+	}
+
+	/**
+	 * Minify CSS.
+	 *
+	 * @param string $css CSS to minify.
+	 * @return array|string|string[]|null
+	 */
+	public function minify( string $css ) {
+		return preg_replace(
+			[
+				'#("(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\')|/\*(?!!)(?>.*?\*/)|^\s*|\s*$#s',
+				'#("(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\'|/\*(?>.*?\*/))|\s*+;\s*+(})\s*+|\s*+([*$~^|]?+=|[{};,>~]|\s(?![\d.])|!important\b)\s*+|([[(:])\s++|\s++([])])|\s++(:)\s*+(?!(?>[^{}"\']++|"(?:[^"\\\]++|\\\.)*+"|\'(?:[^\'\\\\]++|\\\.)*+\')*+{)|^\s++|\s++\z|(\s)\s+#si',
+				'#(?<=[\s:])(0)(cm|em|ex|in|mm|pc|pt|px|vh|vw|%)#i',
+				'#:(0\s+0|0\s+0\s+0\s+0)(?=[;}]|!important)#i',
+				'#(background-position):0(?=[;}])#i',
+				'#(?<=[\s:,\-])0+\.(\d+)#',
+				'#(/\*(?>.*?\*/))|(?<!content:)([\'"])([a-z_][a-z\d\-_]*?)\2(?=[\s{}\];,])#si',
+				'#(/\*(?>.*?\*/))|(\burl\()([\'"])(\S+?)\3(\))#si',
+				'#(?<=[\s:,\-]\#)([a-f0-6]+)\1([a-f0-6]+)\2([a-f0-6]+)\3#i',
+				'#(?<=[{;])(border|outline):none(?=[;}!])#',
+				'#(/\*(?>.*?\*/))|(^|[{}])[^\s{}]+\{}#s',
+			],
+			[
+				'$1',
+				'$1$2$3$4$5$6$7',
+				'$1',
+				':0',
+				'$1:0 0',
+				'.$1',
+				'$1$3',
+				'$1$2$4$5',
+				'$1$2$3',
+				'$1:0',
+				'$1$2',
+			],
+			$css
+		);
 	}
 }
