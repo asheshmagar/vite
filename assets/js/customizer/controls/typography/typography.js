11093a5e00f2b2bdca2dc6cab6168f81fc7272f2
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-11 10:36:30 +0545

    Refactor typography control

--- assets/js/customizer/controls/typography/typography.js
+++ assets/js/customizer/controls/typography/typography.js
@@ -1,10 +1,34 @@
-import { memo, useState, useMemo, useCallback } from '@wordpress/element';
-import { BaseControl, SelectControl } from '@wordpress/components';
+import { memo, useState, useMemo, RawHTML } from '@wordpress/element';
+import { Popover } from '@wordpress/components';
 import { useDeviceSelector } from '../../hooks';
-import { Tooltip, CustomindRange } from '../../components';
+import { CustomindRange } from '../../components';
 import { __ } from '@wordpress/i18n';
 import Select, { Option } from 'rc-select';
 import { VARIANTS } from '../../constants';
+import { dropdownIcon } from '../../utils';
+import { noop } from 'lodash';
+
+const GOOGLE_FONTS = _VITE_CUSTOMIZER_.googleFonts;
+
+GOOGLE_FONTS.unshift( {
+	id: 'default',
+	family: 'System Default',
+	variants: [ 'regular', '100', '200', '300', '400', '500', '600', '700', '800', '900' ],
+	defVariant: 'regular',
+} );
+
+const TEXT_TRANSFORMS = [
+	{ label: __( 'None', 'vite' ), value: 'none' },
+	{ label: __( 'Capitalize', 'vite' ), value: 'capitalize' },
+	{ label: __( 'Uppercase', 'vite' ), value: 'uppercase' },
+	{ label: __( 'Lowercase', 'vite' ), value: 'lowercase' },
+];
+
+const FONT_STYLES = [
+	{ label: __( 'Normal', 'vite' ), value: 'normal' },
+	{ label: __( 'Italic', 'vite' ), value: 'italic' },
+	{ label: __( 'Oblique', 'vite' ), value: 'oblique' },
+];
 
 export default memo( ( props ) => {
 	const {
@@ -13,34 +37,18 @@ export default memo( ( props ) => {
 			params: {
 				label,
 				description,
-				inputAttrs: {
-					fonts = [],
-					units = {},
-					...inputAttrs
-				},
 			},
 		},
 	} = props;
 	const [ value, setValue ] = useState( setting.get() );
 	const { device, DeviceSelector } = useDeviceSelector();
-
-	const update = ( val, type = 'font-family' ) => {
-		const newValue = { ...value, [ type ]: val };
-		setValue( newValue );
-		setting.set( newValue );
-	};
+	const [ anchor, setAnchor ] = useState( null );
+	const [ isOpen, setIsOpen ] = useState( false );
 
 	const currentFont = useMemo( () => {
-		const family = value?.[ 'font-family' ];
-		return fonts.filter( g => g.family === family )?.[ 0 ] || {};
+		const family = value?.family ?? 'System Default';
+		return GOOGLE_FONTS.filter( g => g.family === family )?.[ 0 ] || {};
 	}, [ value ] );
-
-	const inputIcon = () => (
-		<svg width="10" height="10" viewBox="0 0 24 24" id="magicoon-Filled" xmlns="http://www.w3.org/2000/svg">
-			<path id="chevron-down-Filled-2" data-name="chevron-down-Filled" className="cls-1"
-				d="M21.707,8.707l-9,9a1,1,0,0,1-1.414,0l-9-9A1,1,0,1,1,3.707,7.293L12,15.586l8.293-8.293a1,1,0,1,1,1.414,1.414Z" />
-		</svg>
-	);
 
 	const toWeight = ( variant = '' ) => {
 		const matches = variant.match( /(\d+)/ );
@@ -52,206 +60,201 @@ export default memo( ( props ) => {
 		return variant;
 	};
 
-	// const getInputAttrsProps = useCallback( ( type = 'font-size' ) => {
-	// 	const unit = value?.[ type ]?.[ device ]?.unit || 'px';
-	// 	let min = inputAttrs?.[ device ]?.[ type ]?.min || 16;
-	// 	let max = inputAttrs?.[ device ]?.[ type ]?.max || 200;
-	// 	let step = inputAttrs?.[ device ]?.[ type ]?.step || 1;
-	//
-	// 	if ( [ 'em', 'rem' ].includes( unit ) ) {
-	// 		min = 0;
-	// 		max = 20;
-	// 		step = 0.1;
-	// 	}
-	//
-	// 	if ( [ '%', 'vh' ].includes( unit ) ) {
-	// 		min = 0;
-	// 		max = 100;
-	// 		step = 0.1;
-	// 	}
-	//
-	// 	return {
-	// 		min,
-	// 		max,
-	// 		step,
-	// 	};
-	// }, [ device, value ] );
-
-	const getInputAttrsProps = useCallback( ( type = 'font-size' ) => {
-		const allUnits = inputAttrs?.[ device ]?.[ type ];
-		if ( ! allUnits ) return {};
-		return { ...allUnits };
-	}, [ device ] );
-
-	const FontSize = useMemo( () => {
-		return () => (
-			<CustomindRange
-				value={ value?.[ 'font-size' ]?.[ device ] || {} }
-				onChange={ val => {
-					const newVal = { ...( value?.[ 'font-size' ] || {} ), [ device ]: { ...( value?.[ 'font-size' ]?.[ device ] || {} ), ...val } };
-					update( newVal, 'font-size' );
-				} }
-				units={ units?.[ 'font-size' ] }
-				{ ...getInputAttrsProps() }
-			/>
-		);
-	}, [ value, device ] );
-
-	const LetterSpacing = useMemo( () => {
-		return () => (
-			<CustomindRange
-				value={ value?.[ 'letter-spacing' ]?.[ device ] || {} }
-				onChange={ val => {
-					const newVal = { ...( value?.[ 'letter-spacing' ] || {} ), [ device ]: { ...( value?.[ 'letter-spacing' ]?.[ device ] || {} ), ...val } };
-					update( newVal, 'letter-spacing' );
-				} }
-				units={ units?.[ 'letter-spacing' ] }
-				{ ...getInputAttrsProps( 'letter-spacing' ) }
-			/>
-		);
-	}, [ value, device ] );
-
-	const LineHeight = useMemo( () => {
-		return () => (
-			<CustomindRange
-				value={ value?.[ 'line-height' ]?.[ device ] || {} }
-				onChange={ val => {
-					const newVal = { ...( value?.[ 'line-height' ] || {} ), [ device ]: { ...( value?.[ 'line-height' ]?.[ device ] || {} ), ...val } };
-					update( newVal, 'line-height' );
-				} }
-				units={ units?.[ 'line-height' ] }
-				{ ...getInputAttrsProps( 'letter-spacing' ) }
-			/>
-		);
-	}, [ value, device ] );
-
 	return (
 		<div className="vite-control vite-typography-control">
 			{ label && (
 				<div className="vite-control-head">
 					<span className="customize-control-title">{ label }</span>
-					{
-						description && (
-							<Tooltip>
-								<span className="customize-control-description">{ description }</span>
-							</Tooltip>
-						)
-					}
+					<span
+						role={ 'button' }
+						tabIndex={ -1 }
+						onKeyDown={ noop }
+						style={ { width: 24, height: 24, display: 'grid', placeItems: 'center', cursor: 'pointer', border: '1px solid rgb(117, 117, 117)', borderRadius: 2 } }
+						ref={ setAnchor }
+						onClick={ () => setIsOpen( prev => ! prev ) }
+					>
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18">
+							<path d="M20.1 5.1L16.9 2 6.2 12.7l-1.3 4.4 4.5-1.3L20.1 5.1zM4 20.8h8v-1.5H4v1.5z" />
+						</svg>
+					</span>
 				</div>
 			) }
 			<div className="vite-control-body">
-				<div className="font-family">
-					<BaseControl>
-						<BaseControl.VisualLabel>{ __( 'Font Family' ) }</BaseControl.VisualLabel>
-						<Select value={ value?.[ 'font-family' ] || 'default' } onChange={ val => update( val ) } inputIcon={ inputIcon() } showSearch={ true }>
-							<Option value="default">{ __( 'System Default' ) }</Option>
-							{ ( fonts || [] ).map( g => (
-								<Option key={ g.id } value={ g.family }>{ g.family }</Option>
-							) ) }
-						</Select>
-					</BaseControl>
-				</div>
-				{ currentFont?.subsets && (
-					<div className="subsets">
-						<BaseControl>
-							<BaseControl.VisualLabel>{ __( 'Subsets' ) }</BaseControl.VisualLabel>
-							<Select
-								onChange={ val => update( val, 'subsets' ) }
-								value={ ( value?.subsets || [] ).filter( s => currentFont.subsets.includes( s ) ) }
-								inputIcon={ inputIcon() }
-								showSearch={ false }
-								mode="multiple"
-								showArrow={ true }
-							>
-								{ currentFont.subsets.map( s => (
-									<Option key={ s } value={ s }>{ s }</Option>
-								) ) }
-							</Select>
-						</BaseControl>
-					</div>
+				{ isOpen && (
+					<Popover className="vite-typography-popover" anchorRef={ anchor } anchor={ anchor } position="bottom right" onFocusOutside={ () => setIsOpen( false ) }>
+						<div className="vite-typography">
+							<div className="font-family">
+								<span>{ __( 'Font Family' ) }</span>
+								<Select
+									value={ value?.family || 'System Default' }
+									onChange={ val => {
+										const temp = { ...value, family: val };
+										const variants = ( GOOGLE_FONTS.find( g => g.family === val )?.variants ?? [] ).map( v => toWeight( v ) );
+										if ( value?.weight ) {
+											if ( ! variants.includes( value.weight ) ) {
+												if ( variants.includes( 400 ) ) {
+													temp.weight = 400;
+												} else {
+													temp.weight = variants[ 0 ];
+												}
+											}
+										}
+										setValue( temp );
+										setting.set( temp );
+									} }
+									inputIcon={ dropdownIcon }
+									showSearch={ true }
+								>
+									{ ( GOOGLE_FONTS || [] ).map( g => (
+										<Option key={ g.id } value={ g.family }>{ g.family }</Option>
+									) ) }
+								</Select>
+							</div>
+							{ currentFont?.variants && (
+								<div className="font-variant">
+									<span>{ __( 'Variants' ) }</span>
+									<Select
+										onChange={ val => {
+											const temp = {
+												...value,
+												weight: val,
+											};
+											setValue( temp );
+											setting.set( temp );
+										} }
+										value={ VARIANTS?.[ value?.weight ?? toWeight( currentFont?.defVariant ) ] || '' }
+										inputIcon={ dropdownIcon }
+										showSearch={ false }
+										showArrow={ true }
+									>
+										{ currentFont.variants
+											.filter( v => -1 === v.indexOf( 'italic' ) )
+											.map( v => toWeight( v ) )
+											.filter( ( v, i, a ) => a.indexOf( v ) === i )
+											.map( v => {
+												return (
+													<Option key={ v } value={ v }>
+														{ VARIANTS[ v ] }
+													</Option>
+												);
+											} ) }
+									</Select>
+								</div>
+							) }
+							<div className="font-size">
+								<span>{ __( 'Size' ) }</span>
+								<DeviceSelector dropdown={ false } />
+								<CustomindRange
+									unitPicker="select"
+									value={ value?.size?.[ device ] || '' }
+									onChange={ val => {
+										const temp = {
+											...( value || {} ),
+											size: { ...( value?.size || {} ), [ device ]: val },
+										};
+										setting.set( temp );
+										setValue( temp );
+									} }
+									noUnits={ false }
+									min={ 0 }
+									max={ 500 }
+									step={ 1 }
+									units={ [ 'px', 'em', 'rem' ] }
+									defaultUnit="px"
+								/>
+							</div>
+							<div className="line-height">
+								<span>{ __( 'Line Height' ) }</span>
+								<DeviceSelector dropdown={ false } />
+								<CustomindRange
+									unitPicker="select"
+									value={ value?.lineHeight?.[ device ] || '' }
+									onChange={ val => {
+										const temp = {
+											...( value || {} ),
+											lineHeight: { ...( value?.lineHeight || {} ), [ device ]: val },
+										};
+										setting.set( temp );
+										setValue( temp );
+									} }
+									noUnits={ false }
+									min={ 0 }
+									max={ 500 }
+									step={ 1 }
+									units={ [ '-', 'px', 'em', 'rem' ] }
+									defaultUnit="-"
+								/>
+							</div>
+							<div className="letter-spacing">
+								<span>{ __( 'Letter Spacing' ) }</span>
+								<DeviceSelector dropdown={ false } />
+								<CustomindRange
+									unitPicker="select"
+									value={ value?.letterSpacing?.[ device ] || '' }
+									onChange={ val => {
+										const temp = {
+											...( value || {} ),
+											letterSpacing: { ...( value?.letterSpacing || {} ), [ device ]: val },
+										};
+										setting.set( temp );
+										setValue( temp );
+									} }
+									noUnits={ false }
+									min={ 0 }
+									max={ 500 }
+									step={ 1 }
+									units={ [ 'px', 'em', 'rem' ] }
+									defaultUnit="em"
+								/>
+							</div>
+							<div className="font-style">
+								<span>{ __( 'Style' ) }</span>
+								<Select
+									inputIcon={ dropdownIcon }
+									onChange={ val => {
+										const temp = {
+											...( value || {} ),
+											style: val,
+										};
+										setting.set( temp );
+										setValue( temp );
+									} }
+									value={ value?.style || 'normal' }
+								>
+									{ FONT_STYLES.map( s => (
+										<Option key={ s.value } value={ s.value }>{ s.label }</Option>
+									) ) }
+								</Select>
+							</div>
+							<div className="text-transform">
+								<span>{ __( 'Transform' ) }</span>
+								<Select
+									inputIcon={ dropdownIcon }
+									value={ value?.transform || 'none' }
+									onChange={ val => {
+										const temp = {
+											...( value || {} ),
+											transform: val,
+										};
+										setting.set( temp );
+										setValue( temp );
+									} }
+								>
+									{ TEXT_TRANSFORMS.map( t => (
+										<Option key={ t.value } value={ t.value }>{ t.label }</Option>
+									) ) }
+								</Select>
+							</div>
+
+						</div>
+					</Popover>
 				) }
 			</div>
-			{ currentFont?.variants && (
-				<div className="font-variant">
-					<BaseControl>
-						<BaseControl.VisualLabel>{ __( 'Variants' ) }</BaseControl.VisualLabel>
-						<Select
-							onChange={ val => update( val, 'font-weight' ) }
-							value={ VARIANTS?.[ toWeight( value?.[ 'font-weight' ] || currentFont.defVariant ) ] || '' }
-							inputIcon={ inputIcon() }
-							showSearch={ false }
-							showArrow={ true }
-						>
-							{ currentFont.variants
-								.filter( v => -1 === v.indexOf( 'italic' ) )
-								.map( v => {
-									const weight = toWeight( v );
-									return (
-										<Option key={ weight } value={ v }>
-											{ VARIANTS[ weight ] }
-										</Option>
-									);
-								} ) }
-						</Select>
-					</BaseControl>
+			{ description && (
+				<div className="customize-control-description">
+					<RawHTML>{ description }</RawHTML>
 				</div>
 			) }
-			<div className="font-size">
-				<BaseControl>
-					<BaseControl.VisualLabel>{ __( 'Size' ) }</BaseControl.VisualLabel>
-					<DeviceSelector />
-					<div className="font-size-control">
-						<FontSize />
-					</div>
-				</BaseControl>
-			</div>
-			<div className="line-height">
-				<BaseControl>
-					<BaseControl.VisualLabel>{ __( 'Line Height' ) }</BaseControl.VisualLabel>
-					<DeviceSelector />
-					<div className="line-height-control">
-						<LineHeight />
-					</div>
-				</BaseControl>
-			</div>
-			<div className="letter-spacing">
-				<BaseControl>
-					<BaseControl.VisualLabel>{ __( 'Letter Spacing' ) }</BaseControl.VisualLabel>
-					<DeviceSelector />
-					<div className="letter-spacing-control">
-						<LetterSpacing />
-					</div>
-				</BaseControl>
-			</div>
-			<div className="font-style">
-				<SelectControl
-					value={ value?.[ 'font-style' ] || 'normal' }
-					onChange={ val => update( val, 'font-style' ) }
-					label={ __( 'Style' ) }
-					options={ [
-						{ label: __( 'Normal' ), value: 'normal' },
-						{ label: __( 'Italic' ), value: 'italic' },
-						{ label: __( 'Oblique' ), value: 'oblique' },
-						{ label: __( 'initial' ), value: 'initial' },
-						{ label: __( 'Inherit' ), value: 'inherit' },
-					] }
-				/>
-			</div>
-			<div className="text-transform">
-				<SelectControl
-					value={ value?.[ 'text-transform' ] || 'none' }
-					onChange={ val => update( val, 'text-transform' ) }
-					label={ __( 'Transform' ) }
-					options={ [
-						{ label: __( 'None' ), value: 'none' },
-						{ label: __( 'Capitalize' ), value: 'capitalize' },
-						{ label: __( 'Uppercase' ), value: 'uppercase' },
-						{ label: __( 'Lowercase' ), value: 'lowercase' },
-						{ label: __( 'Initial' ), value: 'initial' },
-						{ label: __( 'Inherit' ), value: 'inherit' },
-					] }
-				/>
-			</div>
 		</div>
 	);
 } );
