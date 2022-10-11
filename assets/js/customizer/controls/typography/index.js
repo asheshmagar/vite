3325931cf9e8eba0f60daec2fe0d104f333fcf64
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-11 10:36:41 +0545

    Add styles for typography control

--- assets/js/customizer/controls/typography/index.js
+++ assets/js/customizer/controls/typography/index.js
@@ -1,5 +1,6 @@
 import { registerControl } from '../../utils';
 import Typography from './typography';
+import './customizer.scss';
 
 export default () => {
 	registerControl( 'vite-typography', Typography );
