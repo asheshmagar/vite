931bb68ee14d642dee549e49c6a1f9174f0ad716
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-11 10:37:24 +0545

    Add dropdown icon util fucntion

---
+++ assets/js/customizer/utils/dropdown-icon.js
@@ -0,0 +1,6 @@
+export default () => (
+	<svg width="10" height="10" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+		<path id="chevron-down-Filled-2" data-name="chevron-down-Filled" className="cls-1"
+			d="M21.707,8.707l-9,9a1,1,0,0,1-1.414,0l-9-9A1,1,0,1,1,3.707,7.293L12,15.586l8.293-8.293a1,1,0,1,1,1.414,1.414Z" />
+	</svg>
+);
--- assets/js/customizer/utils/index.js
+++ assets/js/customizer/utils/index.js
@@ -2,3 +2,4 @@ export { default as registerControl } from './register-control';
 export { default as getObjectValues } from './get-object-values';
 export { default as splitUnit } from './split-unit';
 export { default as isNumeric } from './is-numeric';
+export { default as dropdownIcon } from './dropdown-icon';
