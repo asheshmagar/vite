ec0436210a04dc130d89e08b9105ca6dafae10f3
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-11 10:35:34 +0545

    Range component as dropdown or select
    Separate value and unit

--- assets/js/customizer/components/range/index.js
+++ assets/js/customizer/components/range/index.js
@@ -1,13 +1,14 @@
 import { Button, ButtonGroup, Dropdown, RangeControl } from '@wordpress/components';
 import { memo } from '@wordpress/element';
 import './customizer.scss';
-import { splitUnit } from '../../utils';
+import Select, { Option } from 'rc-select';
+import { dropdownIcon } from '../../utils';
 
 const UNITS = [ '-', 'px', 'em', 'rem', '%' ];
 
 export default memo( ( props ) => {
 	let {
-		value = '',
+		value,
 		onChange,
 		noUnits = true,
 		units = UNITS,
@@ -15,53 +16,73 @@ export default memo( ( props ) => {
 		min = 0,
 		max = 300,
 		step = 1,
+		unitPicker = 'dropdown',
 		...otherProps
 	} = props;
 
-	let [ val = '', unit = '' ] = splitUnit( value );
-	unit = unit || defaultUnit;
-	val = parseFloat( val );
-
-	max = [ 'em', 'rem' ].includes( unit ) ? 20 : ( [ 'vh', '%' ].includes( unit ) ? 100 : max );
-	step = [ 'em', 'rem' ].includes( unit ) ? 0.01 : step;
+	max = ( [ 'em', 'rem', '-' ].includes( value?.unit ) || ( ! value?.unit && ! noUnits && '-' === defaultUnit ) ) ? 20 : ( [ 'vh', '%' ].includes( value?.unit ?? defaultUnit ) ? 100 : max );
+	step = ( [ 'em', 'rem', '-' ].includes( value?.unit ) || ( ! value?.unit && ! noUnits && '-' === defaultUnit ) ) ? 0.01 : step;
 
 	return (
 		<div className="vite-range">
 			<RangeControl
-				value={ ! isNaN( val ) ? val : undefined }
-				onChange={ v => onChange( v + ( ! noUnits ? ( '-' === unit ? '' : unit ) : '' ) ) }
+				value={ noUnits ? value : value?.value }
+				onChange={ v => {
+					if ( noUnits ) {
+						onChange( v );
+					} else {
+						onChange( { ...( value || {} ), value: v } );
+					}
+				} }
 				min={ min }
 				max={ max }
 				step={ step }
 				{ ...otherProps }
 			/>
 			{ ! noUnits && (
-				<Dropdown
-					className="vite-units"
-					position="bottom center"
-					renderToggle={ ( { isOpen, onToggle } ) => (
-						<Button onClick={ onToggle } aria-expanded={ isOpen }>
-							{ unit }
-						</Button>
-					) }
-					renderContent={ ( { onToggle } ) => (
-						<ButtonGroup>
+				<>
+					{ 'dropdown' === unitPicker ? (
+						<Dropdown
+							className="vite-units"
+							position="bottom center"
+							renderToggle={ ( { isOpen, onToggle } ) => (
+								<Button onClick={ onToggle } aria-expanded={ isOpen }>
+									{ value?.unit ?? defaultUnit }
+								</Button>
+							) }
+							renderContent={ ( { onToggle } ) => (
+								<ButtonGroup>
+									{ units.map( u => (
+										<Button
+											className={ `vite-unit${ ( value?.unit ?? defaultUnit ) === u ? ' is-primary' : '' }` }
+											key={ u }
+											onClick={ ( e ) => {
+												e.stopPropagation();
+												onToggle();
+												onChange( { ...( value || {} ), unit: u } );
+											} }
+										>
+											{ u }
+										</Button>
+									) ) }
+								</ButtonGroup>
+							) }
+						/>
+					) : (
+						<Select inputIcon={ dropdownIcon() } className="vite-units" value={ value?.unit ?? defaultUnit } onChange={ u => {
+							onChange( { ...( value || {} ), unit: u } );
+						} }>
 							{ units.map( u => (
-								<Button
-									className={ `vite-unit${ unit === u ? ' is-primary' : '' }` }
+								<Option
+									className={ `vite-unit${ ( value?.unit ?? defaultUnit ) === u ? ' is-primary' : '' }` }
 									key={ u }
-									onClick={ ( e ) => {
-										e.stopPropagation();
-										onToggle();
-										onChange( `${ val ?? '0' }${ u }` );
-									} }
 								>
 									{ u }
-								</Button>
+								</Option>
 							) ) }
-						</ButtonGroup>
+						</Select>
 					) }
-				/>
+				</>
 			) }
 		</div>
 	);
