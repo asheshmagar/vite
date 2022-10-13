c097b830a0770233a3768472b07586cf6cc673d3
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-13 10:02:19 +0545

    Change default exports

--- assets/js/customizer/components/index.js
+++ assets/js/customizer/components/index.js
@@ -1,6 +1,6 @@
 export { default as Tooltip } from './tooltip';
-export { default as CustomindColorPicker } from './color-picker';
-export { default as CustomindRange } from './range';
+export { default as ViteColorPicker } from './color-picker';
+export { default as ViteRange } from './range';
 export { default as UnitPicker } from './unit';
 export { default as Popover } from './popover';
 export { default as Reset } from './reset';
