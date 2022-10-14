a29aca71afb483dcbe5dba80cd3be7b3d4952744
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-14 23:13:46 +0545

    Add core function to parse smart tags

--- inc/Core.php
+++ inc/Core.php
@@ -101,4 +101,27 @@ class Core {
 
 		return $defaults[ $id ];
 	}
+
+	/**
+	 * Parse smart tags.
+	 *
+	 * @param string $content Content.
+	 * @return array|string|string[]
+	 */
+	public function parse_smart_tags( string $content = '' ) {
+		$smart_tags = [
+			'{{site-title}}' => get_bloginfo( 'name' ),
+			'{{site-url}}'   => home_url(),
+			'{{year}}'       => date_i18n( 'Y' ),
+			'{{date}}'       => gmdate( 'Y-m-d' ),
+			'{{time}}'       => gmdate( 'H:i:s' ),
+			'{{datetime}}'   => gmdate( 'Y-m-d H:i:s' ),
+		];
+
+		foreach ( $smart_tags as $tag => $value ) {
+			$content = str_replace( $tag, $value, $content );
+		}
+
+		return $content;
+	}
 }
