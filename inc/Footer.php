1f149a7c386736ef9b3be717968a085f590908dd
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-19 15:40:32 +0545

    Basic Footer and Header setup

--- inc/Footer.php
+++ inc/Footer.php
@@ -1,27 +1,83 @@
 <?php
+/**
+ * Footer.
+ *
+ * @package vite
+ * @since 1.0.0
+ */
 
 namespace Vite;
 
+/**
+ * Footer.
+ */
 class Footer {
 
 	/**
-	 * Print footer.
+	 * Render footer.
 	 *
 	 * @return void
 	 */
-	public function render_footer_bar() {
-		$content = vite( 'customizer' )->get_setting( 'footer-bar-html', '' );
-		$content = vite( 'core' )->parse_smart_tags( $content );
+	public function render_footer() {
+		$footer_configs = apply_filters(
+			'vite_footer_elements',
+			[
+				'top'    => [],
+				'middle' => [],
+				'bottom' => [
+					'1' => [ 'html' ],
+				],
+			]
+		);
 		?>
-
+		<footer id="colophon" class="site-footer">
+			<?php
+			foreach ( $footer_configs as $row => $cols ) {
+				$is_row_empty = array_reduce(
+					array_values( $cols ),
+					function( $acc, $curr ) {
+						return empty( $curr );
+					},
+					false
+				);
+				if ( $is_row_empty || empty( $cols ) ) {
+					continue;
+				}
+				?>
+				<div data-row="<?php echo esc_attr( $row ); ?>">
+					<div class="container">
+						<?php foreach ( $cols as $col => $elements ) : ?>
+							<div data-col="<?php echo esc_attr( $col ); ?>">
+								<?php foreach ( $elements as $element ) : ?>
+									<?php preg_match_all( '!\d+!', $element, $matches ); ?>
+									<div data-element="<?php echo esc_attr( $element ); ?>">
+										<?php get_template_part( 'template-parts/footer/footer', $element ); ?>
+									</div>
+								<?php endforeach; ?>
+							</div>
+						<?php endforeach; ?>
+					</div>
+				</div>
+				<?php
+			}
+			?>
+		</footer>
 		<?php
 	}
 
-	public function render_footer() {
+	/**
+	 * Print footer html.
+	 *
+	 * @return void
+	 */
+	public function render_footer_html() {
+		$default = vite( 'customizer' )->get_defaults()['footer-html'];
+		$content = vite( 'customizer' )->get_setting( 'footer-html', $default );
+		$content = vite( 'core' )->parse_smart_tags( $content );
 		?>
-		<footer class="site-footer vite-site-footer">
-
-		</footer>
+		<div class="footer-html">
+			<?php echo do_shortcode( $content ); ?>
+		</div>
 		<?php
 	}
 }
--- inc/Header.php
+++ inc/Header.php
@@ -19,58 +19,56 @@ class Header {
 	 * @return void
 	 */
 	public function render_header() {
-		$header_configs = apply_filters(
-			'theme_header_elements',
-			[
-				'top'    => [
-					'left'   => [],
-					'center' => [],
-					'right'  => [],
-				],
-				'main'   => [
-					'left'   => [ 'logo' ],
-					'center' => [],
-					'right'  => [ 'primary-navigation' ],
-				],
-				'bottom' => [
-					'left'   => [],
-					'center' => [],
-					'right'  => [],
-				],
-			]
-		);
+		$default        = vite( 'customizer' )->get_defaults()['header'];
+		$header_configs = vite( 'customizer' )->get_setting( 'header', $default );
 
 		?>
-		<header id="mast-head" class="site-header vite-site-header">
-			<div class="container">
+		<header id="mast-head" class="site-header">
 			<?php
 			foreach ( $header_configs as $row => $cols ) {
-				$is_row_empty = array_reduce(
-					array_values( $cols ),
-					function( $acc, $curr ) {
-						return empty( $curr );
-					},
-					false
+				$cols_with_content = array_filter(
+					$cols,
+					function( $col ) {
+						return ! empty( $col );
+					}
 				);
-				if ( $is_row_empty ) {
+
+				if ( empty( $cols_with_content ) ) {
 					continue;
 				}
+
+				$col_keys  = array_keys( $cols_with_content );
+				$col_count = count( $col_keys );
+				$cols_attr = $col_count . ':' . implode( ':', $col_keys );
+
+				do_action( 'vite_before_header_row', $row, $cols_with_content );
 				?>
-				<div class="site-header-row vite-header-row" data-row="<?php echo esc_attr( $row ); ?>">
-					<?php foreach ( $cols as $col => $elements ) : ?>
-						<?php if ( ! empty( $elements ) ) : ?>
-							<div class="site-header-col" data-col="<?php echo esc_attr( $col ); ?>">
-								<?php foreach ( $elements as $element ) : ?>
-									<?php get_template_part( 'template-parts/header/header', $element ); ?>
-								<?php endforeach; ?>
-							</div>
-						<?php endif; ?>
-					<?php endforeach; ?>
-				</div>
-				<?php
+					<div data-cols="<?php echo esc_attr( $cols_attr ); ?>" data-row="<?php echo esc_attr( $row ); ?>">
+						<div class="container">
+							<?php foreach ( $cols as $col => $elements ) : ?>
+								<?php
+								if ( empty( $elements ) && (
+										( 1 === $col_count && in_array( 'center', $col_keys, true ) ) ||
+										( 2 === $col_count && ! in_array( 'center', $col_keys, true ) )
+									)
+								) {
+									continue;
+								}
+								?>
+								<div data-col="<?php echo esc_attr( $col ); ?>">
+									<?php foreach ( $elements as $element ) : ?>
+										<div data-element="header-<?php echo esc_attr( $element ); ?>">
+											<?php get_template_part( 'template-parts/header/header', $element ); ?>
+										</div>
+									<?php endforeach; ?>
+								</div>
+							<?php endforeach; ?>
+						</div>
+					</div>
+					<?php
+					do_action( 'vite_after_header_row', $row, $cols_with_content );
 			}
 			?>
-			</div>
 		</header>
 		<?php
 	}
@@ -82,7 +80,7 @@ class Header {
 	 */
 	public function render_header_logo() {
 		?>
-		<div class="site-branding vite-site-branding">
+		<div class="site-branding">
 			<?php the_custom_logo(); ?>
 			<?php if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title vite-site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -103,14 +101,14 @@ class Header {
 	 */
 	public function render_header_primary_navigation() {
 		$args = [
-			'theme_location'  => 'primary',
+			'vite_location'   => 'primary',
 			'menu_id'         => 'primary-menu',
 			'menu_class'      => 'primary-menu menu',
 			'container_class' => 'primary-menu-container',
 			'fallback_cb'     => [ $this, 'primary_navigation_fallback_cb' ],
 		];
 		?>
-		<nav id="site-navigation" class="main-navigation">
+		<nav data-element="header-primary-navigation" id="site-navigation" class="main-navigation">
 			<?php wp_nav_menu( $args ); ?>
 		</nav>
 		<?php
@@ -134,6 +132,50 @@ class Header {
 				);
 				?>
 			</ul>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Search form.
+	 *
+	 * @return void
+	 */
+	public function render_header_search() {
+		?>
+		<div class="header-search">
+			<?php vite( 'icon' )->get_icon( 'magnifying-glass', [ 'echo' => true ] ); ?>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Render header html.
+	 *
+	 * @return void
+	 */
+	public function render_header_html() {
+		$default = vite( 'customizer' )->get_defaults()['header-html'];
+		$content = vite( 'customizer' )->get_setting( 'header-html', $default );
+		?>
+		<div class="header-html">
+			<?php echo do_shortcode( $content ); ?>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Render header button.
+	 *
+	 * @return void
+	 */
+	public function render_header_button() {
+		$defaults    = vite( 'customizer' )->get_defaults();
+		$button_text = vite( 'customizer' )->get_setting( 'header-button-text', $defaults['header-button-text'] );
+		$button_url  = vite( 'customizer' )->get_setting( 'header-button-url', $defaults['header-button-url'] );
+		?>
+		<div class="header-button">
+			<a href="<?php echo esc_url( $button_url ); ?>" class="button"><?php echo esc_html( $button_text ); ?></a>
 		</div>
 		<?php
 	}
