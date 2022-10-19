6d44a333bdd0a3b5c84d1f070a3a46a0b3375877
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-19 15:40:13 +0545

    Change class name

--- inc/PageHeader.php
+++ inc/PageHeader.php
@@ -29,7 +29,7 @@ class PageHeader {
 		?>
 		<div class="page-header-wrap">
 			<header class="page-header">
-				<div class=vite-container>
+				<div class="container">
 					<?php
 					foreach ( $page_header_elements as $page_header_element ) {
 						get_template_part( 'template-parts/page-header/page-header', $page_header_element );
