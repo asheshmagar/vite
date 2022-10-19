b2fbb16011b7efadd5d2ab5fdf83e4a799f4f9f5
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-19 15:36:23 +0545

    Add template parts for footer

---
+++ template-parts/footer/footer-html.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * The template for displaying the footer html.
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+vite( 'footer' )->render_footer_html();
---
+++ template-parts/footer/footer.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * The template for displaying the footer
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+vite( 'footer' )->render_footer();
