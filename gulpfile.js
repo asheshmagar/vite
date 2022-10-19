b1384e20f70e6d53be3475ccf83ceaddb4e2c93b
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-19 15:44:22 +0545

    Change path and fetch latest fontawesome

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ const fetchGoogleFonts = () => request( 'https://google-webfonts-helper.herokuap
 			.sort( function( a, b ) {
 				return a.family.localeCompare( b.family );
 			} );
-		fs.writeFile( 'src/controls/google-fonts.json', JSON.stringify( fonts, null, 2 ), function( err ) {
+		fs.writeFile( 'assets/json/google-fonts.json', JSON.stringify( fonts, null, 2 ), function( err ) {
 			if ( ! err ) {
 				// eslint-disable-next-line no-console
 				console.log( 'Google fonts updated!' );
@@ -18,15 +18,15 @@ const fetchGoogleFonts = () => request( 'https://google-webfonts-helper.herokuap
 	}
 } );
 
-const fetchFontAwesome = () => request( 'https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/metadata/icons.json', function( error, response, body ) {
+const fetchFontAwesome = () => request( 'https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/metadata/icons.json', function( error, response, body ) {
 	if ( ! error && response.statusCode === 200 ) {
 		const icons = JSON.parse( body );
 		const newIcons = Object.keys( icons ).reduce( function( acc, crr ) {
 			const svg = icons[ crr ].svg?.brands || icons[ crr ].svg?.solid;
-			acc[ crr ] = `<svg class="${ crr }" xmlns="http://www.w3.org/2000/svg" viewBox="${ svg.viewBox.join( ' ' ) }"><path fill="currentColor" d="${ svg.path }"></path></svg>`;
+			acc[ crr ] = `<svg class="${ crr } %s" height="%d" width="%d" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="${ svg.viewBox.join( ' ' ) }"><path fill="currentColor" d="${ svg.path }"></path></svg>`;
 			return acc;
 		}, {} );
-		fs.writeFile( 'src/controls/icons.json', JSON.stringify( newIcons, null, 2 ), function( err ) {
+		fs.writeFile( 'assets/json/font-awesome.json', JSON.stringify( newIcons, null, 2 ), function( err ) {
 			if ( ! err ) {
 				// eslint-disable-next-line no-console
 				console.log( 'Fontawesome library updated!' );
