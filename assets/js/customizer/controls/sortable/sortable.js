1c460d9aefe913a12abfe84c881833787dc50145
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-25 12:05:59 +0545

    Allow sortable control for nesting

--- assets/js/customizer/controls/sortable/sortable.js
+++ assets/js/customizer/controls/sortable/sortable.js
@@ -1,6 +1,41 @@
 import { memo, useState, RawHTML } from '@wordpress/element';
-import { Button } from '@wordpress/components';
 import { ReactSortable } from 'react-sortablejs';
+import { isArray } from 'lodash';
+import { sprintf } from '@wordpress/i18n';
+
+const SortableItem = ( { item, innerItems = false, toggleItem, sort } ) => {
+	const [ open, setOpen ] = useState( false );
+	return (
+		<li className="vite-sortable-item" data-sort-disabled={ sort ? undefined : true }>
+			<div className="vite-sortable-item-actions">
+				<RawHTML
+					className="vite-visibility"
+					onClick={ toggleItem }
+				>
+					{ sprintf(
+						item?.visible ?
+							window?._VITE_CUSTOMIZER_?.icons?.eye :
+							window?._VITE_CUSTOMIZER_?.icons?.[ 'eye-slash' ],
+						'vite-icon',
+						14,
+						14
+					) }
+				</RawHTML>
+				<div className="vite-label">{ item.label }</div>
+				{ !! innerItems && (
+					<RawHTML className="vite-toggle" onClick={ () => setOpen( prev => ! prev ) }>
+						{ sprintf( window?._VITE_CUSTOMIZER_?.icons?.[ 'caret-down' ], 'vite-icon', 14, 14 ) }
+					</RawHTML>
+				) }
+			</div>
+			{ ( !! innerItems && open ) && (
+				<div className="vite-sortable-item-inner">
+					{ innerItems() }
+				</div>
+			) }
+		</li>
+	);
+};
 
 export default memo( ( props ) => {
 	const {
@@ -12,6 +47,8 @@ export default memo( ( props ) => {
 				choices,
 				inputAttrs: {
 					sort = true,
+					idWithInnerItems = false,
+					innerItems = [],
 				},
 			},
 			setting,
@@ -19,24 +56,41 @@ export default memo( ( props ) => {
 	} = props;
 
 	const [ value, setValue ] = useState( () => {
-		const val = setting.get();
+		let val = setting.get();
+
+		if ( ! isArray( val ) ) {
+			val = [];
+		}
+
 		if ( ! val?.length ) {
-			return Object.entries( choices ).map( ( [ key, v ] ) => ( {
-				id: key,
-				label: v,
+			val = choices.map( ( c ) => ( {
+				...c,
 				visible: false,
+				items: idWithInnerItems && c.id?.startsWith( idWithInnerItems ) ?
+					innerItems.map( i => ( {
+						...i,
+						visible: false,
+					} ) ) :
+					undefined,
+				chosen: false,
+				selected: false,
 			} ) );
 		}
 
-		if ( val.some( v => ! v?.id ) ) {
-			return val.map( v => ( {
-				id: v,
-				label: choices[ v ],
-				visible: true,
-			} ) );
-		}
-
-		return val;
+		return val.map( v => ( {
+			...v,
+			label: choices.find( c => v?.id?.startsWith( c?.id ) )?.label,
+			items: idWithInnerItems && v?.id?.startsWith( idWithInnerItems ) ?
+				innerItems.map( i => ( {
+					...i,
+					visible: v?.items.find( ii => i?.id === ii?.id )?.visible ?? false,
+					chosen: false,
+					selected: false,
+				} ) ) :
+				undefined,
+			chosen: false,
+			selected: false,
+		} ) );
 	} );
 
 	return (
@@ -69,27 +123,79 @@ export default memo( ( props ) => {
 					} }
 				>
 					{ value.map( ( item, idx, arr ) => (
-						<li key={ item.id } className="vite-sortable-item" data-sort-disabled={ sort ? undefined : true }>
-							<Button
-								onClick={ () => {
-									const temp = [ ...arr ];
-									temp[ idx ] = {
-										...temp[ idx ],
-										visible: ! temp[ idx ].visible,
-									};
-									setValue( temp );
-									setting.set( temp );
-								} }
-								iconSize={ 20 }
-								icon={ item?.visible ? 'visibility' : 'hidden' }
-							/>
-							<span>{ item.label }</span>
-							{ !! sort && (
-								<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-									<path d="M8 7h2V5H8v2zm0 6h2v-2H8v2zm0 6h2v-2H8v2zm6-14v2h2V5h-2zm0 8h2v-2h-2v2zm0 6h2v-2h-2v2z" />
-								</svg>
-							) }
-						</li>
+						<SortableItem
+							key={ item.id }
+							item={ item }
+							sort={ sort }
+							toggleItem={ () => {
+								const temp = [ ...arr ];
+								temp[ idx ] = {
+									...temp[ idx ],
+									visible: ! temp[ idx ].visible,
+								};
+								setValue( temp );
+								setting.set( temp );
+							} }
+							innerItems={ idWithInnerItems && item.id?.startsWith( idWithInnerItems ) && item?.items ? () => {
+								return (
+									<ReactSortable
+										forceFallback={ true }
+										fallbackClass="vite-sortable-fallback"
+										sort={ !! sort }
+										disabled={ ! sort }
+										animation={ 150 }
+										tag="ul"
+										className={ `vite-sortable` }
+										list={ item.items }
+										setList={ ( v ) => {
+											const temp = [ ...arr ];
+											if ( JSON.stringify( temp[ idx ]?.items ) !== JSON.stringify( v ) ) {
+												temp[ idx ] = {
+													...temp[ idx ],
+													items: v,
+												};
+												setValue( temp );
+												setting.set( temp );
+											}
+										} }
+									>
+										{ item.items.map( i => {
+											return (
+												<li key={ i.id } className="vite-sortable-item">
+													<div className="vite-sortable-item-actions">
+														<RawHTML
+															className="vite-visibility"
+															onClick={ () => {
+																const temp = [ ...arr ];
+																temp[ idx ] = {
+																	...temp[ idx ],
+																	items: temp[ idx ].items.map( ii => ( {
+																		...ii,
+																		visible: ii.id === i.id ? ! ii.visible : ii.visible,
+																	} ) ),
+																};
+																setValue( temp );
+																setting.set( temp );
+															} }
+														>
+															{ sprintf(
+																i?.visible ?
+																	window?._VITE_CUSTOMIZER_?.icons?.eye :
+																	window?._VITE_CUSTOMIZER_?.icons?.[ 'eye-slash' ],
+																'vite-icon',
+																14,
+																14
+															) }
+														</RawHTML>
+														<div className="vite-label">{ i.label }</div>
+													</div>
+												</li>
+											);
+										} ) }
+									</ReactSortable>
+								);
+							} : false }
+						/>
 					) ) }
 				</ReactSortable>
 			</div>
