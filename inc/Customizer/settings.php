aabf87c667a40e69283b93b5340fa93490512611
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-25 12:16:56 +0545

    Add settings

--- inc/Customizer/settings.php
+++ inc/Customizer/settings.php
@@ -39,7 +39,19 @@ $section_settings = [
 		'name'     => 'vite[header-builder]',
 		'type'     => 'vite-builder-section',
 		'panel'    => 'vite[header]',
-		'label'    => esc_html__( 'Header', 'customind' ),
+		'label'    => __( 'Header', 'vite' ),
+		'priority' => 10,
+	),
+	array(
+		'name'     => 'vite[archive]',
+		'type'     => 'section',
+		'label'    => __( 'Archive/Blog', 'vite' ),
+		'priority' => 10,
+	),
+	array(
+		'name'     => 'vite[single]',
+		'type'     => 'section',
+		'label'    => __( 'Single Post', 'vite' ),
 		'priority' => 10,
 	),
 ];
@@ -50,45 +62,45 @@ $control_settings = [
 		'type'        => 'control',
 		'section'     => 'vite[global-colors]',
 		'control'     => 'vite-color',
-		'label'       => esc_html__( 'Global Palette', 'customind' ),
+		'label'       => __( 'Global Palette', 'vite' ),
 		'priority'    => 10,
 		'input_attrs' => [
 			'colors' => [
 				[
 					'id'    => '--global--color--1',
-					'label' => esc_html__( 'Color 1', 'customind' ),
+					'label' => __( 'Color 1', 'vite' ),
 				],
 				[
 					'id'    => '--global--color--2',
-					'label' => esc_html__( 'Color 2', 'customind' ),
+					'label' => __( 'Color 2', 'vite' ),
 				],
 				[
 					'id'    => '--global--color--3',
-					'label' => esc_html__( 'Color 3', 'customind' ),
+					'label' => __( 'Color 3', 'vite' ),
 				],
 				[
 					'id'    => '--global--color--4',
-					'label' => esc_html__( 'Color 4', 'customind' ),
+					'label' => __( 'Color 4', 'vite' ),
 				],
 				[
 					'id'    => '--global--color--5',
-					'label' => esc_html__( 'Color 5', 'customind' ),
+					'label' => __( 'Color 5', 'vite' ),
 				],
 				[
 					'id'    => '--global--color--6',
-					'label' => esc_html__( 'Color 6', 'customind' ),
+					'label' => __( 'Color 6', 'vite' ),
 				],
 				[
 					'id'    => '--global--color--7',
-					'label' => esc_html__( 'Color 7', 'customind' ),
+					'label' => __( 'Color 7', 'vite' ),
 				],
 				[
 					'id'    => '--global--color--8',
-					'label' => esc_html__( 'Color 8', 'customind' ),
+					'label' => __( 'Color 8', 'vite' ),
 				],
 				[
 					'id'    => '--global--color--9',
-					'label' => esc_html__( 'Color 9', 'customind' ),
+					'label' => __( 'Color 9', 'vite' ),
 				],
 			],
 		],
@@ -101,7 +113,7 @@ $control_settings = [
 		'type'      => 'control',
 		'section'   => 'vite[global-typography]',
 		'control'   => 'vite-typography',
-		'label'     => esc_html__( 'Base', 'customind' ),
+		'label'     => __( 'Base', 'vite' ),
 		'priority'  => 10,
 		'selectors' => [ 'body' ],
 		'transport' => 'postMessage',
@@ -111,7 +123,7 @@ $control_settings = [
 		'type'      => 'control',
 		'section'   => 'vite[global-typography]',
 		'control'   => 'vite-typography',
-		'label'     => esc_html__( 'Heading 1 (h1)', 'customind' ),
+		'label'     => __( 'Heading 1 (h1)', 'vite' ),
 		'priority'  => 10,
 		'selectors' => [ 'h1' ],
 		'transport' => 'postMessage',
@@ -121,7 +133,7 @@ $control_settings = [
 		'type'      => 'control',
 		'section'   => 'vite[global-typography]',
 		'control'   => 'vite-typography',
-		'label'     => esc_html__( 'Heading 2 (h2)', 'customind' ),
+		'label'     => __( 'Heading 2 (h2)', 'vite' ),
 		'priority'  => 10,
 		'selectors' => [ 'h2' ],
 		'transport' => 'postMessage',
@@ -131,7 +143,7 @@ $control_settings = [
 		'type'      => 'control',
 		'section'   => 'vite[global-typography]',
 		'control'   => 'vite-typography',
-		'label'     => esc_html__( 'Heading 3 (h3)', 'customind' ),
+		'label'     => __( 'Heading 3 (h3)', 'vite' ),
 		'priority'  => 10,
 		'selectors' => [ 'h3' ],
 		'transport' => 'postMessage',
@@ -141,7 +153,7 @@ $control_settings = [
 		'type'      => 'control',
 		'section'   => 'vite[global-typography]',
 		'control'   => 'vite-typography',
-		'label'     => esc_html__( 'Heading 4 (h4)', 'customind' ),
+		'label'     => __( 'Heading 4 (h4)', 'vite' ),
 		'priority'  => 10,
 		'selectors' => [ 'h4' ],
 		'transport' => 'postMessage',
@@ -151,7 +163,7 @@ $control_settings = [
 		'type'      => 'control',
 		'section'   => 'vite[global-typography]',
 		'control'   => 'vite-typography',
-		'label'     => esc_html__( 'Heading 5 (h5)', 'customind' ),
+		'label'     => __( 'Heading 5 (h5)', 'vite' ),
 		'priority'  => 10,
 		'selectors' => [ 'h5' ],
 		'transport' => 'postMessage',
@@ -161,7 +173,7 @@ $control_settings = [
 		'type'      => 'control',
 		'section'   => 'vite[global-typography]',
 		'control'   => 'vite-typography',
-		'label'     => esc_html__( 'Heading 6 (h6)', 'customind' ),
+		'label'     => __( 'Heading 6 (h6)', 'vite' ),
 		'priority'  => 10,
 		'selectors' => [ 'h6' ],
 		'transport' => 'postMessage',
@@ -169,54 +181,38 @@ $control_settings = [
 	[
 		'name'        => 'vite[header]',
 		'type'        => 'control',
-		'default'     => [
-			'top'    => [
-				'left'   => [],
-				'center' => [],
-				'right'  => [],
-			],
-			'main'   => [
-				'left'   => [ 'logo' ],
-				'center' => [],
-				'right'  => [ 'primary-navigation', 'search' ],
-			],
-			'bottom' => [
-				'left'   => [],
-				'center' => [],
-				'right'  => [],
-			],
-		],
+		'default'     => vite( 'customizer' )->get_defaults()['header'],
 		'control'     => 'vite-builder',
-		'label'       => esc_html__( 'Header builder', 'vite' ),
+		'label'       => __( 'Header builder', 'vite' ),
 		'section'     => 'vite[header-builder]',
 		'priority'    => 30,
 		'choices'     => [
 			'logo'                 => [
-				'name'    => esc_html__( 'Logo', 'vite' ),
+				'name'    => __( 'Logo', 'vite' ),
 				'section' => 'label_tagline',
 			],
 			'primary-navigation'   => [
-				'name'    => esc_html__( 'Primary Navigation', 'vite' ),
+				'name'    => __( 'Primary Navigation', 'vite' ),
 				'section' => 'vite_primary_navigation',
 			],
 			'secondary-navigation' => [
-				'name'    => esc_html__( 'Secondary Navigation', 'vite' ),
+				'name'    => __( 'Secondary Navigation', 'vite' ),
 				'section' => 'vite_secondary_navigation',
 			],
 			'search'               => [
-				'name'    => esc_html__( 'Search', 'vite' ),
+				'name'    => __( 'Search', 'vite' ),
 				'section' => 'vite_header_search',
 			],
 			'button'               => [
-				'name'    => esc_html__( 'Button', 'vite' ),
+				'name'    => __( 'Button', 'vite' ),
 				'section' => 'vite_header_button',
 			],
 			'social'               => [
-				'name'    => esc_html__( 'Social', 'vite' ),
+				'name'    => __( 'Social', 'vite' ),
 				'section' => 'vite_header_social',
 			],
 			'html'                 => [
-				'name'    => esc_html__( 'HTML', 'vite' ),
+				'name'    => __( 'HTML', 'vite' ),
 				'section' => 'vite_header_html',
 			],
 		],
@@ -245,6 +241,128 @@ $control_settings = [
 			'container_inclusive' => true,
 			'render_callback'     => function() {
 				get_template_part( 'template-parts/header/header', '' );
+			},
+		],
+	],
+	[
+		'name'        => 'vite[archive-elements]',
+		'type'        => 'control',
+		'control'     => 'vite-sortable',
+		'label'       => __( 'Archive elements', 'vite' ),
+		'section'     => 'vite[archive]',
+		'choices'     => [
+			[
+				'id'    => 'title',
+				'label' => __( 'Title', 'vite' ),
+			],
+			[
+				'id'    => 'featured-image',
+				'label' => __( 'Featured Image', 'vite' ),
+			],
+			[
+				'id'    => 'meta',
+				'label' => __( 'Meta', 'vite' ),
+			],
+			[
+				'id'    => 'excerpt',
+				'label' => __( 'Excerpt', 'vite' ),
+			],
+			[
+				'id'    => 'read-more',
+				'label' => __( 'Read more', 'vite' ),
+			],
+		],
+		'default'     => vite( 'customizer' )->get_defaults()['archive-elements'],
+		'input_attrs' => [
+			'idWithInnerItems' => 'meta',
+			'innerItems'       => [
+				[
+					'id'    => 'author',
+					'label' => __( 'Author', 'vite' ),
+				],
+				[
+					'id'    => 'published-date',
+					'label' => __( 'Published Date', 'vite' ),
+				],
+				[
+					'id'    => 'updated-date',
+					'label' => __( 'Updated Date', 'vite' ),
+				],
+				[
+					'id'    => 'categories',
+					'label' => __( 'Categories', 'vite' ),
+				],
+				[
+					'id'    => 'tags',
+					'label' => __( 'Tags', 'vite' ),
+				],
+				[
+					'id'    => 'comments',
+					'label' => __( 'Comments', 'vite' ),
+				],
+			],
+		],
+		'transport'   => 'postMessage',
+		'partial'     => [
+			'selector'            => '.vite-posts',
+			'container_inclusive' => true,
+			'render_callback'     => function() {
+				vite( 'core' )->the_loop();
+			},
+		],
+	],
+	[
+		'name'        => 'vite[single-header-elements]',
+		'type'        => 'control',
+		'control'     => 'vite-sortable',
+		'label'       => __( 'Header/Title elements', 'vite' ),
+		'section'     => 'vite[single]',
+		'choices'     => [
+			[
+				'id'    => 'title',
+				'label' => __( 'Title', 'vite' ),
+			],
+			[
+				'id'    => 'meta',
+				'label' => __( 'Meta', 'vite' ),
+			],
+			[
+				'id'    => 'breadcrumbs',
+				'label' => __( 'Breadcrumbs', 'vite' ),
+			],
+		],
+		'default'     => vite( 'customizer' )->get_defaults()['single-header-elements'],
+		'input_attrs' => [
+			'idWithInnerItems' => 'meta',
+			'innerItems'       => [
+				[
+					'id'    => 'author',
+					'label' => __( 'Author', 'vite' ),
+				],
+				[
+					'id'    => 'published-date',
+					'label' => __( 'Published Date', 'vite' ),
+				],
+				[
+					'id'    => 'updated-date',
+					'label' => __( 'Updated Date', 'vite' ),
+				],
+				[
+					'id'    => 'categories',
+					'label' => __( 'Categories', 'vite' ),
+				],
+				[
+					'id'    => 'tags',
+					'label' => __( 'Tags', 'vite' ),
+				],
+			],
+		],
+		'transport'   => 'postMessage',
+		'partial'     => [
+			'selector'            => '.vite-single',
+			'container_inclusive' => true,
+			'render_callback'     => function() {
+				get_template_part( 'template-parts/content/content', 'single' );
 			},
 		],
 	],
