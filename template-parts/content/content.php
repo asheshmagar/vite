d23f4b9328e6811a32406d186f2cfb767681ddb1
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-10-28 15:24:52 +0545

    Add data element attributes

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -12,9 +12,18 @@
 defined( 'ABSPATH' ) || exit;
 
 $elements = vite( 'customizer' )->get_setting( 'archive-elements' );
+
+$visible_elements = array_filter(
+	$elements,
+	function( $element ) {
+		return $element['visible'];
+	}
+);
+$first_element    = reset( $visible_elements );
+$last_element     = end( $visible_elements );
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> data-first-element="<?php echo esc_attr( $first_element['id'] ?? 'false' ); ?>" data-last-element="<?php echo esc_attr( $last_element['id'] ?? 'false' ); ?>">
 	<div class="post-wrap">
 		<?php vite( 'entry-elements' )->render_entry_elements( $elements ); ?>
 	</div>
