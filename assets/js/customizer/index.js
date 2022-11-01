b9d15c6108fefd1c940b3858e4f8f15f67fae27d
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-11-01 18:24:48 +0545

    Add script to handle conditions

--- assets/js/customizer/index.js
+++ assets/js/customizer/index.js
@@ -1,321 +1,8 @@
 import $ from 'jquery';
-import _ from 'lodash';
 import * as controls from './controls';
 import './customizer.scss';
 
 const api = wp.customize;
-
-/**
- * Extend sections and panels.
- */
-{
-	api.bind( 'pane-contents-reflowed', () => {
-		const panels = [],
-			sections = [];
-
-		api.section.each( section => {
-			if ( 'vite-section' === section.params.type && !! section.params?.section ) {
-				sections.push( section );
-			}
-		} );
-
-		sections.sort( api.utils.prioritySort ).reverse();
-
-		$.each( sections, ( i, section ) => {
-			const parentContainer = $( `#sub-accordion-section-${ section.params.section }` );
-			parentContainer.children( '.section-meta' ).after( section.headContainer );
-		} );
-
-		api.panel.each( panel => {
-			if ( 'vite-panel' === panel.params.type && !! panel.params?.panel ) {
-				panels.push( panel );
-			}
-		} );
-
-		panels.sort( api.utils.prioritySort ).reverse();
-
-		$.each( panels, ( i, panel ) => {
-			const parentContainer = $( `#sub-accordion-panel-${ panel.params.panel }` );
-			parentContainer.children( '.section-meta' ).after( panel.headContainer );
-		} );
-	} );
-
-	const panelEmbed = api.Panel.prototype.embed,
-		panelIsContextuallyActive = api.Panel.prototype.isContextuallyActive,
-		panelAttachEvents = api.Panel.prototype.attachEvents;
-
-	api.Panel = api.Panel.extend( {
-		attachEvents() {
-			if ( 'vite-panel' !== this.params.type || ! this.params?.panel ) {
-				panelAttachEvents.call( this );
-				return;
-			}
-			panelAttachEvents.call( this );
-
-			const panel = this;
-
-			panel.expanded.bind( expanded => {
-				const parent = api.panel( panel.params.panel );
-
-				if ( expanded ) {
-					parent.contentContainer.addClass( 'current-panel-parent' );
-				} else {
-					parent.contentContainer.removeClass( 'current-panel-parent' );
-				}
-			} );
-
-			panel.container
-				.find( '.customize-panel-back' )
-				.off( 'click keydown' )
-				.on( 'click keydown', evt => {
-					if ( api.utils.isKeydownButNotEnterEvent( evt ) ) {
-						return;
-					}
-					evt.preventDefault();
-					if ( panel.expanded() ) {
-						api.panel( panel.params.panel ).expand();
-					}
-				} );
-		},
-		embed() {
-			if ( 'vite-panel' !== this.params.type || ! this.params?.section ) {
-				panelEmbed.call( this );
-				return;
-			}
-
-			panelEmbed.call( this );
-
-			const panel = this;
-			const parentContainer = $( `#sub-accordion-panel-${ panel.params.panel }` );
-
-			parentContainer.append( panel.headContainer );
-		},
-		isContextuallyActive() {
-			if ( 'vite-panel' !== this.params.type ) {
-				return panelIsContextuallyActive.call( this );
-			}
-
-			const panel = this;
-			const children = this._children( 'panel', 'section' );
-			let activeCount = 0;
-
-			api.panel.each( child => {
-				if ( child.params?.panel !== panel.id ) return;
-				children.push( child );
-			} );
-
-			children.sort( api.utils.prioritySort );
-
-			_( children ).each( child => {
-				if ( child.active() && child.isContextuallyActive() ) {
-					activeCount += 1;
-				}
-			} );
-
-			return 0 !== activeCount;
-		},
-	} );
-
-	const sectionEmbed = api.Section.prototype.embed,
-		sectionIsContextuallyActive = api.Section.prototype.isContextuallyActive,
-		sectionAttachEvents = api.Section.prototype.attachEvents;
-
-	api.Section = api.Section.extend( {
-		attachEvents() {
-			const section = this;
-
-			if ( 'vite-section' !== section.params.type || ! section.params?.section ) {
-				sectionAttachEvents.call( this );
-				return;
-			}
-			sectionAttachEvents.call( this );
-
-			section.expanded.bind( expanded => {
-				const parent = api.section( section.params.section );
-
-				if ( expanded ) {
-					parent.contentContainer.addClass( 'current-section-parent' );
-				} else {
-					parent.contentContainer.removeClass( 'current-section-parent' );
-				}
-			} );
-
-			section.container
-				.find( '.customize-section-back' )
-				.off( 'click keydown' )
-				.on( 'click keydown', evt => {
-					if ( api.utils.isKeydownButNotEnterEvent( evt ) ) {
-						return;
-					}
-					evt.preventDefault();
-					if ( section.expanded() ) {
-						api.section( section.params.section ).expand();
-					}
-				} );
-		},
-		embed() {
-			if ( 'vite-section' !== this.params.type ) {
-				sectionEmbed.call( this );
-				return;
-			}
-
-			sectionEmbed.call( this );
-
-			const section = this;
-			const parentContainer = $( `#sub-accordion-section-${ section.params.section }` );
-
-			parentContainer.append( section.headContainer );
-		},
-		isContextuallyActive() {
-			if ( 'vite-section' !== this.params.type || ! this.params?.section ) {
-				return sectionIsContextuallyActive.call( this );
-			}
-
-			const section = this;
-			const children = this._children( 'section', 'control' );
-			let activeCount = 0;
-
-			api.section.each( child => {
-				if ( child.params?.section !== section.id ) return;
-				children.push( child );
-			} );
-
-			children.sort( api.utils.prioritySort );
-
-			_( children ).each( child => {
-				if ( child?.isContextuallyActive ) {
-					if ( child.active() && child.isContextuallyActive() ) {
-						activeCount += 1;
-					}
-				} else {
-					if ( child.active() ) { // eslint-disable-line no-lonely-if
-						activeCount += 1;
-					}
-				}
-			} );
-
-			return 0 !== activeCount;
-		},
-	} );
-}
-
-// Initialize builder and its panel and section.
-{
-	api.bind( 'ready', () => {
-		const builderPanelSection = {};
-
-		api.control.each( control => {
-			if ( control.section() ) {
-				const section = api.section( control.section() );
-				if ( section?.panel() && 'vite-builder-section' === section.params?.type ) {
-					const panel = api.panel( section.panel() );
-					if ( panel?.id && 'vite-builder-panel' === panel.params?.type ) {
-						builderPanelSection[ panel.id ] = [
-							...( builderPanelSection?.[ panel.id ] || [] ),
-							section.id,
-						];
-					}
-				}
-			}
-		} );
-
-		const initBuilder = ( panelId, sectionIds ) => {
-			const panel = api.panel( panelId );
-
-			if ( ! panel || ! sectionIds?.length ) return;
-
-			sectionIds = _.uniq( sectionIds );
-
-			const selectors = [];
-
-			for ( const sectionId of sectionIds ) {
-				const section = api.section( sectionId );
-
-				if ( section ) {
-					const contentContainer = section.contentContainer;
-					const headContainer = section.headContainer;
-
-					selectors.push( `[id="sub-accordion-section-${ section.id }"]` );
-					headContainer.attr( 'data-navigator-hidden', 'true' );
-					contentContainer.find( '.section-meta' ).addClass( 'hidden' ).hide();
-
-					panel.expanded.bind( ( isExpanded ) => {
-						if ( ! section.controls()?.length ) return;
-
-						_.each( section.controls(), control => {
-							if ( 'resolved' === control.deferred.embedded.state() ) return;
-							control.renderContent();
-							control.deferred.embedded.resolve();
-							control.container.trigger( 'init' );
-						} );
-
-						if ( isExpanded ) {
-							$( `#sub-accordion-panel-${ panelId } li.control-section` ).attr( 'data-navigator-hidden', 'true' );
-							$( 'body' ).attr( 'data-builder', 'active' );
-						} else {
-							$( 'body' ).removeAttr( 'data-builder' );
-						}
-					} );
-				}
-			}
-			let styles = '';
-			if ( selectors?.length > 0 ) {
-				for ( const selector of selectors ) {
-					styles += `[data-builder="active"] .in-sub-panel:not(.section-open) ul${ selector }{transform:none;height:auto;visibility:visible;top: 75px;}`;
-				}
-			}
-			$( 'head' ).append( `<style>${ styles }</style>` );
-		};
-
-		if ( ! _.isEmpty( builderPanelSection ) ) {
-			for ( const panel in builderPanelSection ) {
-				if ( panel && builderPanelSection?.[ panel ] ) {
-					initBuilder( panel, builderPanelSection[ panel ] || [] );
-				}
-			}
-		}
-	} );
-}
-
-{
-	api.bind( 'ready', () => {
-		api.state.create( 'viteTab' );
-		api.state( 'viteTab' ).set( 'general' );
-
-		const $controls = $( '#customize-theme-controls' );
-
-		const focusSection = ( sectionId ) => {
-			const section = api.section( sectionId );
-			if ( section ) {
-				const container = section.contentContainer[ 0 ];
-				container.addClass( 'vite-prevent-transition' );
-				setTimeout( () => {
-					section.focus();
-				}, 10 );
-				setTimeout( () => {
-					container.removeClass( 'vite-prevent-transition' ).removeClass( 'busy' );
-					container.css( 'top', '' );
-				}, 300 );
-			}
-		};
-
-		$controls.on( 'click', '.vite-tab', function( e ) {
-			e.preventDefault();
-			const target = $( this ).attr( 'data-target' );
-			api.state( 'viteTab' ).set( $( this ).attr( 'data-tab' ) );
-			if ( target ) {
-				focusSection( target );
-			}
-		} );
-
-		api.state( 'viteTab' ).bind( () => {
-			const tab = api.state( 'viteTab' ).get();
-			$( '.vite-tab' ).removeClass( 'active' ).filter( `.vite-${ tab }-tab` ).addClass( 'active' );
-		} );
-
-		$controls.on( 'click', '.customize-section-back', () => api.state( 'viteTab' ).set( 'general' ) );
-	} );
-}
 
 {
 	api.bind( 'preview-ready', () => {
@@ -323,6 +10,147 @@ const api = wp.customize;
 			if ( ! res?.viteDynamicCSS ) return;
 			const $style = $( '#vite-dynamic-css' );
 			$style.html( res.viteDynamicCSS );
+		} );
+	} );
+}
+
+{
+	api.bind( 'ready', () => {
+		const toStyles = values => {
+			let style = ':root{';
+			for ( const key in values ) {
+				style += `${ key }:${ values[ key ] };`;
+			}
+			style += '}';
+			$( 'style#vite-root-css' ).remove();
+			$( 'head' ).append( `<style id="vite-root-css">${ style }</style>` );
+		};
+		let values = {};
+		[ 'global-palette', 'link-colors' ].forEach( control => {
+			const val = api( `vite[${ control }]` ).get();
+			if ( val ) {
+				values = { ...values, ...val };
+				toStyles( values );
+			}
+			api( `vite[${ control }]`, value => {
+				value.bind( to => {
+					values = { ...values, ...to };
+					toStyles( values );
+				} );
+			} );
+		} );
+
+		api.state.create( 'vite-tab' );
+		api.state( 'vite-tab' ).set( 'general' );
+		$( '#customize-theme-controls' ).on( 'click', '.customize-section-back', () => api.state( 'vite-tab' ).set( 'general' ) );
+
+		const getControl = ( id ) => {
+			if ( 'vite-tab' === id ) {
+				return api.state( 'vite-tab' );
+			}
+			return api( id );
+		};
+
+		const analyzeConditions = ( conditions = {} ) => {
+			const relation = conditions.relation ?? 'AND';
+			const terms = conditions.terms ?? [];
+
+			if ( ! terms.length ) return true;
+
+			const results = terms.map( term => {
+				const { name, operator = '===', value } = term;
+				const control = getControl( name );
+				if ( ! control ) return true;
+				const setting = control.get();
+				switch ( operator.trim() ) {
+					case '===':
+						return setting === value;
+					case '!==':
+						return setting !== value;
+					case '==':
+						return setting == value; // eslint-disable-line
+					case '!=':
+						return setting != value; // eslint-disable-line
+					case '>':
+						return setting > value;
+					case '>=':
+						return setting >= value;
+					case '<':
+						return setting < value;
+					case '<=':
+						return setting <= value;
+					case 'contains':
+						return setting.includes( value );
+					case '!contains':
+						return ! setting.includes( value );
+					case 'in':
+						return value.includes( setting );
+					case '!in':
+						return ! value.includes( setting );
+					default:
+						return false;
+				}
+			} );
+
+			if ( relation === 'AND' ) {
+				return results.every( result => result );
+			}
+
+			if ( relation === 'OR' ) {
+				return results.some( result => result );
+			}
+
+			return true;
+		};
+
+		const analyzeCondition = ( condition = {} ) => {
+			const results = Object.entries( condition ).map( ( [ key, value ] ) => {
+				const control = getControl( key.replace( '!', '' ) );
+				if ( ! control ) return true;
+				const setting = control.get();
+				if ( key.startsWith( '!' ) || key.endsWith( '!' ) ) {
+					return setting !== value;
+				}
+				return setting === value;
+			} );
+
+			return results.every( result => result );
+		};
+
+		[ 'conditions', 'condition' ].forEach( c => {
+			if ( window._VITE_CUSTOMIZER_?.[ c ] ) {
+				for ( const id in window._VITE_CUSTOMIZER_[ c ] ) {
+					const init = ( element ) => {
+						const control = getControl( id );
+						if ( ! control ) return;
+						const state = _VITE_CUSTOMIZER_[ c ][ id ];
+						const result = () => c === 'conditions' ? analyzeConditions( state ) : analyzeCondition( state );
+						const setActiveState = () => {
+							element.active.set( result() );
+						};
+						control.bind( setActiveState );
+						element.active.validate = result;
+						for ( const i in state ) {
+							if ( 'terms' in state ) {
+								for ( const term of state.terms ) {
+									const setting = getControl( term.name );
+									if ( setting ) {
+										setting.bind( setActiveState );
+									}
+								}
+							} else {
+								const setting = getControl( i.replace( '!', '' ) );
+								if ( setting ) {
+									setting.bind( setActiveState );
+								}
+							}
+						}
+						setActiveState();
+					};
+
+					api.control( id, init );
+				}
+			}
 		} );
 	} );
 }
