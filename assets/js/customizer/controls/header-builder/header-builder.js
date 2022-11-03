ba23969861657f8ba712244751bcc37cfc10c545
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-11-03 09:35:50 +0545

    Add settings button to navigate to section

--- assets/js/customizer/controls/header-builder/header-builder.js
+++ assets/js/customizer/controls/header-builder/header-builder.js
@@ -1,9 +1,9 @@
-import { memo, useState, useCallback, useEffect, useRef, useMemo } from '@wordpress/element';
+import { memo, useState, useCallback, useEffect, Fragment } from '@wordpress/element';
 import { cloneDeep, isEqual } from 'lodash';
-import { Button } from '@wordpress/components';
 import { usePortal, useDeviceSelector } from '../../hooks';
 import { ReactSortable } from 'react-sortablejs';
-import { sprintf } from '@wordpress/i18n';
+import { sprintf, __ } from '@wordpress/i18n';
+import { ButtonGroup, Button } from '@wordpress/components';
 
 export default memo( ( props ) => {
 	const {
@@ -14,21 +14,23 @@ export default memo( ( props ) => {
 					areas = {},
 				},
 				choices,
+				section,
 			},
 			setting,
-			section,
 		},
 		customizer,
 	} = props;
 
 	const [ value, setValue ] = useState( setting.get() );
-	// const [ open, setOpen ] = useState( false );
+	const [ open, setOpen ] = useState( false );
 	const Portal = usePortal( document.querySelector( '.wp-full-overlay' ) );
-	const { device, setDevice } = useDeviceSelector();
-	//
-	const update = ( row, area, items ) => {
+	let { device, setDevice } = useDeviceSelector();
+
+	device = 'desktop' === device ? 'desktop' : 'mobile';
+
+	const update = ( row, area, items, screen = 'desktop' ) => {
 		const newValue = cloneDeep( value );
-		newValue[ device ][ row ][ area ] = items;
+		newValue[ screen ][ row ][ area ] = items;
 		if ( ! isEqual( newValue, value ) ) {
 			setValue( newValue );
 			setting.set( newValue );
@@ -44,116 +46,156 @@ export default memo( ( props ) => {
 		}
 	};
 
-	const getAreaItems = useCallback( ( row, area ) => value?.[ device ]?.[ row ]?.[ area ] || [], [ value ] );
+	const getAreaItems = useCallback( ( row, area, d ) => value?.[ d ]?.[ row ]?.[ area ] || [], [ value, device ] );
 
-	const items = Object.values( value?.[ 'desktop' === device ? 'desktop' : 'mobile' ] ).reduce( ( acc, v ) => {
-		for ( const val of Object.values( v ) ) {
-			if ( val?.length ) {
-				for ( const item of val ) {
-					acc = [ ...acc, item.id ];
-				}
-			}
+	useEffect( () => {
+		const sec = customizer.section( section );
+		const panel = sec?.panel();
+		if ( panel ) {
+			customizer.panel( panel ).expanded.bind( setOpen );
 		}
-		return acc;
 	}, [] );
 
-	const availableItems = Object.entries( choices ).map( ( [ item, val ] ) => ( {
-		id: item,
-		label: val.name,
-		section: val.section,
-		disabled: items.includes( item ),
-	} ) );
-
 	return (
-		<div className="vite-control vite-header-builder-control">
-			<div className="vite-builder-available-items">
-				{ availableItems.map( ( item ) => (
-					<ReactSortable
-						forceFallback={ true }
-						list={ [ item ] }
-						setList={ () => {} }
-						key={ item.id }
-						className="vite-builder-available-item"
-						fallbackClass="vite-builder-item-fallback"
-						ghostClass="vite-builder-item-placeholder"
-						chosenClass="vite-builder-item-chosen"
-						dragClass="vite-builder-item-dragging"
-						disabled={ item.disabled }
-						group={ {
-							name: `vite-builder[${ id }]`,
-							pull: 'clone',
-							put: false,
-						} }
-					>
-						<div className="vite-available-item">
-							<span className="vite-builder-item-title">
-								{ item.label }
-							</span>
-							<span
-								className="vite-builder-item-handle"
-								dangerouslySetInnerHTML={ {
-									__html: sprintf( window?._VITE_CUSTOMIZER_?.icons?.[ item.disabled ? 'chevron-right' : 'arrows-up-down-left-right' ], 'vite-icon', 10, 10 ),
-								} }
-							/>
-						</div>
-					</ReactSortable>
-				) ) }
-			</div>
-			<Portal>
-				<div className="vite-builder-portal">
-					<div className="vite-builder-portal-header">
+		<Portal>
+			<div className="vite-builder-portal" data-builder-open={ open } data-builder={ id }>
+				<div className="vite-builder-portal-header">
+					<div className="vite-builder-portal-header-left">
+						<ButtonGroup>
+							{ [ {
+								id: 'desktop',
+								label: __( 'Desktop', 'vite' ),
+							}, {
+								id: 'mobile',
+								label: __( 'Tablet / Mobile', 'vite' ),
+							} ].map( d => (
+								<Button
+									key={ d.id }
+									onClick={ () => setDevice( 'mobile' === d.id ? 'tablet' : d.id ) }
+									icon={ 'mobile' === d.id ? 'tablet' : d.id }
+									className={ device === d.id ? 'active' : '' }
+								>
+									{ d.label }
+								</Button>
+							) ) }
+						</ButtonGroup>
 					</div>
-					<div className="vite-builder-portal-content">
+				</div>
+				<div className="vite-builder-portal-content">
+					<div className="vite-builder-portal-content-inner" data-desktop={ device }>
 						{ [ 'desktop', 'mobile' ].map( d => (
-							<div key={ d } className="vite-builder-rows" style={ { display: d !== device ? 'none' : undefined } }>
-								{
-									Object.entries( areas ).map( ( [ row, columns = { } ] ) => (
-										<div key={ row } data-row={ row } className="vite-builder-row">
-											<div className="vite-builder-cols">
-												{ Object.keys( columns ).map( ( area ) => (
-													<div className="vite-col" data-col={ area } key={ area }>
-														<ReactSortable
-															forceFallback={ true }
-															list={ getAreaItems( row, area ) }
-															setList={ ( list ) => update( row, area, list ) }
-															className="vite-builder-area"
-															fallbackClass="vite-builder-item-fallback"
-															ghostClass="vite-builder-item-placeholder"
-															chosenClass="vite-builder-item-chosen"
-															dragClass="vite-builder-item-dragging"
-															group={ {
-																name: `vite-builder[${ id }]`,
-																pull: true,
-																put: true,
+							<Fragment key={ d }>
+								{ 'mobile' === d && (
+									<div className="vite-builder-mobile-offset" style={ { display: 'mobile' !== device ? 'none' : undefined } }>
+										<div className="vite-builder-mobile-offset-inner">
+											<ReactSortable
+												forceFallback={ true }
+												list={ value.offset ?? [] }
+												setList={ ( list ) => {
+													const val = cloneDeep( value );
+													val.offset = list;
+													if ( ! isEqual( val, value ) ) {
+														setValue( val );
+														setting.set( val );
+													}
+												} }
+												animation={ 150 }
+												className="vite-builder-area"
+												fallbackClass="vite-builder-component-fallback"
+												ghostClass="vite-builder-component-placeholder"
+												chosenClass="vite-builder-component-chosen"
+												dragClass="vite-builder-component-dragging"
+												group={ {
+													name: `vite-builder[${ id }]`,
+													pull: true,
+													put: true,
+												} }
+											>
+												{ value?.offset?.map( ( item ) => (
+													// eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
+													<div className="vite-builder-component" key={ item.id } onClick={ choices?.offset?.[ item.id ]?.section ? () => customizer.section( choices[ item.id ].section )?.focus() : null }>
+														<span className="vite-builder-component-title">
+															{ choices?.[ device ]?.[ item.id ]?.name }
+														</span>
+														<span
+															role="button"
+															tabIndex={ -1 }
+															onKeyDown={ () => {} }
+															className="vite-builder-component-handle"
+															dangerouslySetInnerHTML={ {
+																__html: sprintf( window?._VITE_CUSTOMIZER_?.icons?.xmark, 'vite-icon', 10, 10 ),
 															} }
-														>
-															{ getAreaItems( row, area ).map( ( item ) => (
-																<div className="vite-builder-item" key={ item.id }>
-																	<span className="vite-builder-item-title">
-																		{ choices?.[ item.id ]?.name }
-																	</span>
-																	{ /* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */ }
-																	<span
-																		className="vite-builder-item-handle"
-																		dangerouslySetInnerHTML={ {
-																			__html: sprintf( window?._VITE_CUSTOMIZER_?.icons?.xmark, 'vite-icon', 10, 10 ),
-																		} }
-																		onClick={ () => remove( row, area, item ) }
-																	/>
-																</div>
-															) ) }
-														</ReactSortable>
+															onClick={ () => {
+																const val = cloneDeep( value );
+																val.offset = val.offset.filter( ( i ) => i.id !== item.id );
+																if ( ! isEqual( val, value ) ) {
+																	setValue( val );
+																	setting.set( val );
+																}
+															} }
+														/>
 													</div>
 												) ) }
-											</div>
+											</ReactSortable>
+											<Button className="vite-builder-cta" icon={ 'admin-generic' } onClick={ customizer.section( `vite[header-offset]` ) ? customizer.section( `vite[header-offset]` ).focus() : undefined } />
 										</div>
-									) )
-								}
-							</div>
+									</div>
+								) }
+								<div key={ d } className="vite-builder-rows" style={ { display: d !== device ? 'none' : undefined } }>
+									{
+										Object.entries( areas ).map( ( [ row = '', columns = { } ] ) => (
+											<div key={ row } data-row={ row } className="vite-builder-row">
+												<div className="vite-builder-cols">
+													{ Object.keys( columns ).map( ( area ) => (
+														<div className="vite-col" data-col={ area } key={ area }>
+															<ReactSortable
+																forceFallback={ true }
+																list={ getAreaItems( row, area, d ) }
+																setList={ ( list ) => update( row, area, list, d ) }
+																className="vite-builder-area"
+																fallbackClass="vite-builder-component-fallback"
+																ghostClass="vite-builder-component-placeholder"
+																chosenClass="vite-builder-component-chosen"
+																dragClass="vite-builder-component-dragging"
+																group={ {
+																	name: `vite-builder[${ id }]`,
+																	pull: true,
+																	put: true,
+																} }
+																animation={ 150 }
+															>
+																{ getAreaItems( row, area, d ).map( ( item ) => (
+																	// eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
+																	<div className="vite-builder-component" key={ item.id } onClick={ choices?.[ device ]?.[ item.id ]?.section ? () => customizer.section( choices?.[ device ]?.[ item.id ].section )?.focus() : null }>
+																		<span className="vite-builder-component-title">
+																			{ choices?.[ device ]?.[ item.id ]?.name }
+																		</span>
+																		<span
+																			role="button"
+																			tabIndex={ -1 }
+																			onKeyDown={ () => {} }
+																			className="vite-builder-component-handle"
+																			dangerouslySetInnerHTML={ {
+																				__html: sprintf( window?._VITE_CUSTOMIZER_?.icons?.xmark, 'vite-icon', 10, 10 ),
+																			} }
+																			onClick={ () => remove( row, area, item ) }
+																		/>
+																	</div>
+																) ) }
+															</ReactSortable>
+														</div>
+													) ) }
+												</div>
+												<Button className="vite-builder-cta" icon={ 'admin-generic' } onClick={ customizer.section( `vite[header-${ row }-row]` ) ? () => customizer.section( `vite[header-${ row }-row]` ).focus() : undefined } />
+											</div>
+										) )
+									}
+								</div>
+							</Fragment>
 						) ) }
 					</div>
 				</div>
-			</Portal>
-		</div>
+			</div>
+		</Portal>
 	);
 } );
