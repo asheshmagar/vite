ce2fa1a1ce89e3d8bf3cfcbfb452b4ca9cd4e9a7
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-11-17 18:57:45 +0545

    Remove unnecessary exports

--- assets/js/customizer/components/index.js
+++ assets/js/customizer/components/index.js
@@ -1,6 +1,5 @@
 export { default as Tooltip } from './tooltip';
 export { default as ViteColorPicker } from './color-picker';
 export { default as ViteRange } from './range';
-export { default as UnitPicker } from './unit';
 export { default as Popover } from './popover';
 export { default as Reset } from './reset';
