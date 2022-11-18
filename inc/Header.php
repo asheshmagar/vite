bad8d5839eafebdfd8b26b1cae9c76b2f6741934
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-11-18 16:36:29 +0545

    Tweak header button and html methods to accommodate new settings

--- inc/Header.php
+++ inc/Header.php
@@ -36,7 +36,7 @@ class Header {
 		$default        = vite( 'customizer' )->get_defaults()['header'];
 		$header_configs = vite( 'customizer' )->get_setting( 'header', $default )['mobile'];
 
-		do_action( 'vite_before_desktop_header' );
+		do_action( 'vite_before_mobile_header' );
 		?>
 		<div data-device-mobile>
 			<?php
@@ -73,7 +73,12 @@ class Header {
 								<div data-col="<?php echo esc_attr( $col ); ?>">
 									<?php foreach ( $elements as $element ) : ?>
 										<div data-element="header-<?php echo esc_attr( $element['id'] ); ?>">
-											<?php get_template_part( 'template-parts/header/header', $element['id'] ); ?>
+											<?php
+												preg_match( '/\d+$/', $element['id'], $matches );
+												$element_id = preg_replace( '/(_|-)\d+/', '', $element['id'] );
+												$type       = $matches[0] ?? null;
+											?>
+											<?php get_template_part( 'template-parts/header/header', $element_id, [ 'type' => $type ] ); ?>
 										</div>
 									<?php endforeach; ?>
 								</div>
@@ -135,7 +140,12 @@ class Header {
 						<div data-col="<?php echo esc_attr( $col ); ?>">
 							<?php foreach ( $elements as $element ) : ?>
 								<div data-element="header-<?php echo esc_attr( $element['id'] ); ?>">
-									<?php get_template_part( 'template-parts/header/header', $element['id'] ); ?>
+									<?php
+										preg_match( '/\d+$/', $element['id'], $matches );
+										$element_id = preg_replace( '/(_|-)\d+/', '', $element['id'] );
+										$type       = $matches[0] ?? null;
+									?>
+									<?php get_template_part( 'template-parts/header/header', $element_id, [ 'type' => $type ] ); ?>
 								</div>
 							<?php endforeach; ?>
 						</div>
@@ -157,17 +167,23 @@ class Header {
 	 * @return void
 	 */
 	public function render_header_logo() {
+		$elements = vite( 'customizer' )->get_setting( 'header-site-branding-elements' );
+		$layout   = vite( 'customizer' )->get_setting( 'header-site-branding-layout' );
 		?>
-		<div class="site-branding">
+		<div class="site-branding" data-layout="<?php echo esc_attr( $layout ); ?>">
 			<?php the_custom_logo(); ?>
-			<?php if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title vite-site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title vite-site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php if ( in_array( $elements, [ 'logo-title', 'logo-title-description' ], true ) ) : ?>
+				<?php if ( is_front_page() && is_home() ) : ?>
+					<h1 class="site-title vite-site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php else : ?>
+					<p class="site-title vite-site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php endif; ?>
 			<?php endif; ?>
-<!--			--><?php // if ( get_bloginfo( 'description', 'display' ) || is_customize_preview() ) : ?>
-<!--				<p class="site-description vite-site-description">--><?php // bloginfo( 'description' ); ?><!--</p>-->
-<!--			--><?php // endif; ?>
+			<?php if ( 'logo-title-description' === $elements ) : ?>
+				<?php if ( get_bloginfo( 'description', 'display' ) || is_customize_preview() ) : ?>
+				<p class="site-description vite-site-description"><?php bloginfo( 'description' ); ?></p>
+			<?php endif; ?>
+			<?php endif; ?>
 		</div>
 		<?php
 	}
@@ -191,14 +207,20 @@ class Header {
 	 * @return void
 	 */
 	public function render_header_search() {
+		$label            = vite( 'customizer' )->get_setting( 'header-search-label' );
+		$label_visibility = vite( 'customizer' )->get_setting( 'header-search-label-visibility' );
+		$label_position   = vite( 'customizer' )->get_setting( 'header-search-label-position' );
 		?>
 		<div class="search-modal-trigger">
-			<a href="#" class="search-modal-open" data-modal-trigger>
+			<a href="#" class="search-modal-open" data-modal-trigger data-label-visibility="<?php echo esc_attr( implode( ':', $label_visibility ) ); ?>" data-label-position="<?php echo esc_attr( $label_position ); ?>">
+				<?php if ( ! empty( $label ) ) : ?>
+					<span><?php echo esc_html( $label ); ?></span>
+				<?php endif; ?>
 				<?php vite( 'icon' )->get_icon( 'magnifying-glass', [ 'echo' => true ] ); ?>
 			</a>
 		</div>
 		<?php
-		add_action( 'wp_footer', [ $this, 'render_search_modal' ] );
+		add_action( 'wp_footer', [ $this, 'render_search_modal' ], 11 );
 	}
 
 	/**
@@ -231,13 +253,14 @@ class Header {
 	/**
 	 * Render header html.
 	 *
+	 * @param mixed $type HTML type.
 	 * @return void
 	 */
-	public function render_header_html() {
-		$content = vite( 'customizer' )->get_setting( 'header-html' );
+	public function render_header_html( $type = 1 ) {
+		$content = vite( 'customizer' )->get_setting( "header-html-$type" );
 		$content = vite( 'core' )->parse_smart_tags( $content );
 		?>
-		<div class="header-html">
+		<div class="header-html-<?php echo esc_attr( $type ); ?>">
 			<?php echo do_shortcode( $content ); ?>
 		</div>
 		<?php
@@ -246,15 +269,39 @@ class Header {
 	/**
 	 * Render header button.
 	 *
+	 * @param mixed $type Button type.
 	 * @return void
 	 */
-	public function render_header_button() {
-		$defaults    = vite( 'customizer' )->get_defaults();
-		$button_text = vite( 'customizer' )->get_setting( 'header-button-text', $defaults['header-button-text'] );
-		$button_url  = vite( 'customizer' )->get_setting( 'header-button-url', $defaults['header-button-url'] );
+	public function render_header_button( $type = 1 ) {
+		$text      = vite( 'customizer' )->get_setting( "header-button-$type-text" );
+		$url       = vite( 'customizer' )->get_setting( "header-button-$type-url" );
+		$target    = vite( 'customizer' )->get_setting( "header-button-$type-target" );
+		$download  = vite( 'customizer' )->get_setting( "header-button-$type-download" );
+		$sponsored = vite( 'customizer' )->get_setting( "header-button-$type-sponsored" );
+		$nofollow  = vite( 'customizer' )->get_setting( "header-button-$type-nofollow" );
+		$style     = vite( 'customizer' )->get_setting( "header-button-$type-style" );
+
+		$rel = [];
+
+		if ( $nofollow ) {
+			$rel[] = 'nofollow';
+		}
+
+		if ( $sponsored ) {
+			$rel[] = 'sponsored';
+		}
 		?>
-		<div class="header-button">
-			<a href="<?php echo esc_url( $button_url ); ?>" class="button"><?php echo esc_html( $button_text ); ?></a>
+		<div class="header-button-<?php echo esc_attr( $type ); ?>" data-style="<?php echo esc_attr( $style ); ?>">
+			<?php
+			printf(
+				'<a href="%s" class="button" target="%s" %s%s>%s</a>',
+				esc_url( $url ),
+				esc_attr( $target ? '_blank' : '_self' ),
+				esc_attr( ! empty( $rel ) ? 'rel=' . implode( ' ', $rel ) : '' ),
+				esc_attr( $download ? ' download' : '' ),
+				esc_html( $text )
+			);
+			?>
 		</div>
 		<?php
 	}
