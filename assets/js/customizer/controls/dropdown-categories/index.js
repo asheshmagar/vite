cd31cbda4c365a5040e0646a9e3f46b5fdc5e275
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-11-30 18:38:53 +0545

    Remove dropdown categories

--- assets/js/customizer/controls/dropdown-categories/index.js
+++
@@ -1,6 +0,0 @@
-import { registerControl } from '../../utils';
-import DropdownCategories from './dropdown-categories';
-
-export default () => {
-	registerControl( 'vite-dropdown-categories', DropdownCategories );
-};
