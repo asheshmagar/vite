925ffd1cde53bdb5687fdfb1029e27b7dd7cd506
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-11-30 18:24:48 +0545

    Require option files

---
+++ inc/Customizer/options/options.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Options.
+ *
+ * @package Vite
+ */
+
+$directories = [
+	__DIR__ . '/general',
+	__DIR__ . '/header',
+	__DIR__ . '/footer',
+	__DIR__ . '/archive',
+	__DIR__ . '/single',
+];
+
+foreach ( $directories as $directory ) {
+	if ( ! file_exists( $directory ) ) {
+		continue;
+	}
+	$files = scandir( $directory );
+	foreach ( $files as $file ) {
+		if ( in_array( $file, [ '.', '..' ], true ) ) {
+			continue;
+		}
+		$file_path = $directory . '/' . $file;
+		file_exists( $file_path ) && require $file_path;
+	}
+}
