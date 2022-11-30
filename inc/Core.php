8452d7d1b352e98064ae6b35e236ae46362d1b02
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-11-30 18:19:45 +0545

    Add core method to add remove filters and actions

--- inc/Core.php
+++ inc/Core.php
@@ -2,7 +2,7 @@
 /**
  * The loop.
  *
- * @since 1.0.0
+ * @since x.x.x
  * @package Vite
  */
 
@@ -23,27 +23,175 @@ class Core {
 	public $post_types;
 
 	/**
+	 * Adds a callback function to an action hook.
+	 *
+	 * @param string                $hook_name     The name of the filter to add the callback to.
+	 * @param callable|string|array $callback      The callback to be run when the filter is applied.
+	 * @param int                   $priority      Optional. Used to specify the order in which the functions
+	 *                                                  associated with a particular filter are executed.
+	 *                                                  Lower numbers correspond with earlier execution,
+	 *                                                  and functions with the same priority are executed
+	 *                                                  in the order in which they were added to the filter. Default 10.
+	 * @param int                   $accepted_args Optional. The number of arguments the function accepts. Default 1.
+	 *
+	 * @return true Always returns true.
+	 */
+	public function add_action( string $hook_name, $callback, int $priority = 10, int $accepted_args = 1 ) {
+		return add_action( $hook_name, $callback, $priority, $accepted_args );
+	}
+
+	/**
+	 * Adds a callback function to a filter hook.
+	 *
+	 * @param string                $hook_name     The name of the filter to add the callback to.
+	 * @param callable|string|array $callback      The callback to be run when the filter is applied.
+	 * @param int                   $priority      Optional. Used to specify the order in which the functions
+	 *                                                  associated with a particular filter are executed.
+	 *                                                  Lower numbers correspond with earlier execution,
+	 *                                                  and functions with the same priority are executed
+	 *                                                  in the order in which they were added to the filter. Default 10.
+	 * @param int                   $accepted_args Optional. The number of arguments the function accepts. Default 1.
+	 *
+	 * @return true Always returns true.
+	 */
+	public function add_filter( string $hook_name, $callback, int $priority = 10, int $accepted_args = 1 ) {
+		return add_filter( $hook_name, $callback, $priority, $accepted_args );
+	}
+
+	/**
+	 * Removes a callback function from an action hook.
+	 *
+	 * This can be used to remove default functions attached to a specific filter
+	 * hook and possibly replace them with a substitute.
+	 *
+	 * To remove a hook, the `$callback` and `$priority` arguments must match
+	 * when the hook was added. This goes for both filters and actions. No warning
+	 * will be given on removal failure.
+	 *
+	 * @param string                $hook_name The filter hook to which the function to be removed is hooked.
+	 * @param callable|string|array $callback  The callback to be removed from running when the filter is applied.
+	 *                                         This function can be called unconditionally to speculatively remove
+	 *                                         a callback that may or may not exist.
+	 * @param int                   $priority  Optional. The exact priority used when adding the original
+	 *                                                           filter callback. Default 10.
+	 * @return bool Whether the function existed before it was removed.
+	 */
+	public function remove_action( string $hook_name, $callback, int $priority = 10 ): bool {
+		return $this->remove_filter( $hook_name, $callback, $priority );
+	}
+
+	/**
+	 * Removes a callback function from a filter hook.
+	 *
+	 * This can be used to remove default functions attached to a specific filter
+	 * hook and possibly replace them with a substitute.
+	 *
+	 * To remove a hook, the `$callback` and `$priority` arguments must match
+	 * when the hook was added. This goes for both filters and actions. No warning
+	 * will be given on removal failure.
+	 *
+	 * @param string                $hook_name The filter hook to which the function to be removed is hooked.
+	 * @param callable|string|array $callback  The callback to be removed from running when the filter is applied.
+	 *                                         This function can be called unconditionally to speculatively remove
+	 *                                         a callback that may or may not exist.
+	 * @param int                   $priority  Optional. The exact priority used when adding the original
+	 *                                         filter callback. Default 10.
+	 * @return bool Whether the function existed before it was removed.
+	 */
+	public function remove_filter( string $hook_name, $callback, int $priority = 10 ): bool {
+		return remove_filter( $hook_name, $callback, $priority );
+	}
+
+	/**
+	 * Do action with vite as prefix.
+	 *
+	 * @param mixed ...$args Arguments.
+	 * @return void
+	 */
+	public function action( ...$args ) {
+		$handle = array_shift( $args );
+
+		if ( is_null( $handle ) ) {
+			return;
+		}
+
+		$handle = "vite/$handle";
+
+		do_action_ref_array( $handle, array_merge( [], $args ) );
+	}
+
+	/**
+	 * Apply filters with vite as prefix.
+	 *
+	 * @param mixed ...$args Arguments.
+	 * @return mixed|void
+	 */
+	public function filter( ...$args ) {
+		$handle = array_shift( $args );
+
+		if ( is_null( $handle ) ) {
+			return;
+		}
+
+		$action = "vite/$handle";
+
+		return apply_filters_ref_array( $action, array_merge( [], $args ) );
+	}
+
+	/**
 	 * The loop.
 	 *
 	 * @return void
 	 */
 	public function the_loop() {
-		do_action( 'vite_before_the_loop' );
+		/**
+		 * Action: vite/the-loop/start.
+		 *
+		 * Fires before the loop.
+		 *
+		 * @since x.x.x
+		 */
+		$this->action( 'the-loop/start' );
+
 		if ( have_posts() ) {
 			while ( have_posts() ) {
 				the_post();
-				do_action( 'vite_the_loop' );
+
+				/**
+				 * Action: vite/the-loop.
+				 *
+				 * Fires once the post is loaded.
+				 *
+				 * @since x.x.x
+				 * @see TemplateHooks::content()
+				 * @param \WP_Post $post The post object.
+				 */
+				$this->action( 'the-loop', get_post() );
 			}
 		} else {
-			do_action( 'vite_no_posts' );
+
+			/**
+			 * Action: vite/the-loop/end.
+			 *
+			 * Fires when no posts are found.
+			 *
+			 * @since x.x.x
+			 */
+			$this->action( 'the-loop/no-posts' );
 		}
-		do_action( 'vite_after_the_loop' );
+
+		/**
+		 * Fires after the loop.
+		 *
+		 * @since x.x.x
+		 */
+		$this->action( 'the-loop/end' );
 	}
 
 	/**
 	 * Get the current post ID.
 	 *
-	 * @return int
+	 * @return mixed
 	 */
 	public function get_the_id(): int {
 		if ( is_home() && 'page' === get_option( 'show_on_front' ) ) {
@@ -58,7 +206,13 @@ class Core {
 			$post_id = 0;
 		}
 
-		return $post_id;
+		/**
+		 * Filter: vite/the-id.
+		 *
+		 * @since x.x.x
+		 * @param int $post_id The post ID.
+		 */
+		return $this->filter( 'the-id', $post_id );
 	}
 
 	/**
@@ -148,7 +302,7 @@ class Core {
 			],
 			'objects'
 		);
-		$exclude    = apply_filters(
+		$exclude    = $this->filter(
 			'vite_ignored_post_types',
 			[
 				'attachment',
@@ -214,5 +368,30 @@ class Core {
 			},
 			[]
 		);
+	}
+
+	/**
+	 * Get social networks.
+	 *
+	 * @return array|mixed
+	 */
+	public function get_social_networks() {
+		$json            = VITE_ASSETS_DIR . 'json/social-networks.json';
+		$social_networks = [];
+		if ( file_exists( $json ) ) {
+			ob_start();
+			require $json;
+			$social_networks = json_decode( ob_get_clean(), true );
+		}
+		return $social_networks;
+	}
+
+	/**
+	 * Get public path.
+	 *
+	 * @return string
+	 */
+	public function get_public_path(): string {
+		return VITE_ASSETS_URI . 'dist/';
 	}
 }
