74be9c714a6b4c661d3aacad2bf42e4066a3f6a0
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-12-07 10:29:53 +0545

    Change classnames

--- template-parts/navigation/navigation.php
+++ template-parts/navigation/navigation.php
@@ -10,6 +10,8 @@
 defined( 'ABSPATH' ) || exit;
 
 $core = vite( 'core' );
+$prev = get_previous_post();
+$next = get_next_post();
 
 /**
  * Action: vite/single/post-navigation/start
@@ -19,13 +21,44 @@ $core = vite( 'core' );
  * @since 1.0.0
  */
 $core->action( 'single/post-navigation/start' );
-
-the_post_navigation(
-	[
-		'prev_text' => '<span class="nav-subtitle screen-reader-text">' . __( 'Previous Post', 'vite' ) . '</span><span class="nav-icon">' . vite( 'icon' )->get_icon( 'chevron-left', [ 'size' => 10 ] ) . '</span><span class="nav-title">%title</span>',
-		'next_text' => '<span class="nav-subtitle screen-reader-text">' . __( 'Next Post', 'vite' ) . '</span><span class="nav-title">%title</span><span class="nav-icon">' . vite( 'icon' )->get_icon( 'chevron-right', [ 'size' => 10 ] ) . '</span>',
-	]
-);
+?>
+<div class="vite-navigation">
+	<?php if ( $prev ) : ?>
+		<a class="vite-navigation__link vite-navigation__link--prev" href="<?php echo esc_url( get_permalink( $prev->ID ) ); ?>">
+			<span class="screen-reader-text"><?php esc_html_e( 'Previous Post', 'vite' ); ?></span>
+			<span class="vite-navigation__icon">
+			<?php
+			vite( 'icon' )->get_icon(
+				'chevron-left',
+				[
+					'size' => 10,
+					'echo' => true,
+				]
+			);
+			?>
+			</span>
+			<span class="vite-navigation__title"><?php echo esc_html( get_the_title( $prev->ID ) ); ?></span>
+		</a>
+	<?php endif; ?>
+	<?php if ( $next ) : ?>
+		<a class="vite-navigation__link vite-navigation__link--next" href="<?php echo esc_url( get_permalink( $next->ID ) ); ?>">
+			<span class="screen-reader-text"><?php esc_html_e( 'Next Post', 'vite' ); ?></span>
+			<span class="vite-navigation__title"><?php echo esc_html( get_the_title( $next->ID ) ); ?></span>
+			<span class="vite-navigation__icon">
+			<?php
+			vite( 'icon' )->get_icon(
+				'chevron-right',
+				[
+					'size' => 10,
+					'echo' => true,
+				]
+			);
+			?>
+			</span>
+		</a>
+	<?php endif; ?>
+</div>
+<?php
 
 /**
  * Action: vite/single/post-navigation/end
--- template-parts/pagination/pagination.php
+++ template-parts/pagination/pagination.php
@@ -28,6 +28,7 @@ $pagination_args = $core->filter(
 			__( 'Next Page', 'vite' ),
 			vite( 'icon' )->get_icon( is_rtl() ? 'chevron-left' : 'chevron-right', [ 'size' => 12 ] )
 		),
+		'class'     => 'vite-pagination',
 	]
 );
 
@@ -40,7 +41,50 @@ $pagination_args = $core->filter(
  */
 $core->action( 'pagination/start' );
 
-the_posts_pagination( $pagination_args );
+$pagination = paginate_links( $pagination_args );
+
+if ( $pagination ) {
+	$pagination = str_replace( 'class="page-numbers"', 'class="vite-pagination__link"', $pagination );
+	$pagination = str_replace( 'class="page-numbers current"', 'class="vite-pagination__link vite-pagination__link--current"', $pagination );
+	$pagination = str_replace( 'class="next page-numbers"', 'class="vite-pagination__link vite-pagination__link--next"', $pagination );
+	$pagination = str_replace( 'class="prev page-numbers"', 'class="vite-pagination__link vite-pagination__link--prev"', $pagination );
+	?>
+	<nav class="vite-pagination" aria-label="<?php esc_attr_e( 'Posts', 'vite' ); ?>">
+		<h2 class="screen-reader-text"><?php esc_html_e( 'Posts navigation', 'vite' ); ?></h2>
+		<div class="vite-pagination__links">
+			<?php
+				echo wp_kses(
+					$pagination,
+					[
+						'span' => [
+							'class'        => true,
+							'aria-current' => true,
+						],
+						'a'    => [
+							'class' => true,
+							'href'  => true,
+						],
+						'svg'  => [
+							'class'       => true,
+							'aria-hidden' => true,
+							'role'        => true,
+							'xmlns'       => true,
+							'width'       => true,
+							'height'      => true,
+							'viewbox'     => true,
+							'fill'        => true,
+						],
+						'path' => [
+							'd'    => true,
+							'fill' => true,
+						],
+					]
+				);
+			?>
+		</div>
+	</nav>
+	<?php
+}
 
 /**
  * Action: vite/pagination/end
