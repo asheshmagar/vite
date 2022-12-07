85e553e4702a80e1e1aa512f77a7d50cf9df713b
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-12-07 10:34:50 +0545

    Change selector

--- assets/js/frontend/index.js
+++ assets/js/frontend/index.js
@@ -3,15 +3,17 @@ import { $, $$ } from './utils';
 
 import( './dom-ready' ).then( ( { default: domReady } ) => {
 	domReady( () => {
-		const menus = [ ...( $$( '.header-primary-menu, .header-secondary-menu' ) ) ];
+		const menus = [ ...( $$( '.vite-nav--1, .vite-nav--2' ) ) ];
 		if ( menus.length ) {
-			[ ...( $$( '.header-primary-menu, .header-secondary-menu' ) ) ].forEach( ( menu ) => {
+			menus.forEach( ( menu ) => {
 				const links = menu.querySelectorAll( 'a' );
-				const menuItems = menu.querySelectorAll( '.vite-has-sub-menu' );
+				const innerParent = menu.querySelectorAll( '.vite-nav__item--parent .vite-nav__item--parent' );
+				const allParents = menu.querySelectorAll( '.vite-nav__item--parent' );
+				const toggles = menu.querySelectorAll( '.vite-nav__submenu-toggle' );
 
 				const toggleFocus = ( e ) => {
 					let self = e.target;
-					while ( -1 === [ ...self.classList ].findIndex( c => [ 'primary-menu', 'secondary-menu' ].includes( c ) ) ) {
+					while ( -1 === [ ...self.classList ].findIndex( c => 'vite-nav__list' === c ) ) {
 						if ( -1 !== self.className.indexOf( 'focus' ) ) {
 							self.className = self.className.replace( ' focus', '' );
 						} else {
@@ -27,49 +29,51 @@ import( './dom-ready' ).then( ( { default: domReady } ) => {
 					link.addEventListener( 'blur', toggleFocus );
 				}
 
-				for ( const menuItem of menuItems ) {
-					const toggle = menuItem.querySelector( '.vite-sub-menu-toggle' );
-					const submenu = menuItem.querySelector( '.sub-menu' );
+				for ( const toggle of toggles ) {
+					toggle?.addEventListener( 'click', ( e ) => {
+						const parent = e.target.parentNode;
+						if ( parent.classList.contains( 'vite-nav__item--expanded' ) ) {
+							toggle.setAttribute( 'aria-expanded', 'false' );
+							parent.classList.remove( 'vite-nav__item--expanded' );
+						} else {
+							toggle.setAttribute( 'aria-expanded', 'true' );
+							parent.classList.add( 'vite-nav__item--expanded' );
+						}
+						e.preventDefault();
+					} );
+				}
+
+				for ( const parent of allParents ) {
+					parent?.addEventListener( 'focusout', ( e ) => {
+						if ( ! parent.contains( e.relatedTarget ) ) {
+							parent.classList.remove( 'vite-nav__item--expanded' );
+							parent.querySelector( '.vite-nav__submenu-toggle' )?.setAttribute( 'aria-expanded', 'false' );
+						}
+					} );
+				}
+
+				for ( const parent of innerParent ) {
+					const submenu = parent.querySelector( '.vite-nav__submenu' );
+					const icon = parent.querySelector( '.vite-nav__submenu-icon' );
 					const rect = submenu.getBoundingClientRect();
 
 					// Auto position the sub-menu.
 					if ( rect.right > window.innerWidth ) {
-						submenu.dataset.direction = 'left';
-						submenu.closest( '.menu-item' ).dataset.direction = 'left';
+						submenu.classList.add( 'vite-nav__submenu--left' );
+						icon.classList.add( 'vite-nav__submenu-icon--left' );
 					} else {
-						submenu.dataset.direction = 'right';
-						submenu.closest( '.menu-item' ).dataset.direction = 'right';
+						submenu.classList.add( 'vite-nav__submenu--right' );
+						icon.classList.add( 'vite-nav__submenu-icon--right' );
 					}
-
-					// Toggle sub-menu.
-					toggle?.addEventListener( 'click', ( e ) => {
-						const parent = e.target.parentNode;
-						if ( menuItem.dataset.subMenuOpen ) {
-							toggle.setAttribute( 'aria-expanded', 'false' );
-							delete parent.dataset.subMenuOpen;
-						} else {
-							parent.dataset.subMenuOpen = 'true';
-							toggle.setAttribute( 'aria-expanded', 'true' );
-						}
-						e.preventDefault();
-					} );
-
-					// Collapse sub-menu when focus is out.
-					submenu?.addEventListener( 'focusout', ( e ) => {
-						if ( ! menuItem.contains( e.relatedTarget ) ) {
-							delete menuItem.dataset.subMenuOpen;
-							menuItem.querySelector( '.vite-sub-menu-toggle' )?.setAttribute( 'aria-expanded', 'false' );
-						}
-					} );
 				}
 			} );
 		}
 
 		// Handle mobile submenu toggle.
-		const mobileSubmenuToggle = [ ...( $$( '.mobile-menu .vite-sub-menu-toggle' ) ) ];
+		const mobileSubmenuToggle = [ ...( $$( '.vite-nav--3 .vite-nav__submenu-toggle' ) ) ];
 		if ( mobileSubmenuToggle.length ) {
 			mobileSubmenuToggle.forEach( el => {
-				const submenu = el.closest( '.menu-item' )?.querySelector( '.sub-menu' );
+				const submenu = el.closest( '.menu-item' )?.querySelector( '.vite-nav__submenu' );
 				if ( ! submenu ) return;
 				const submenuIcon = el.querySelector( '.vite-icon' );
 				el.addEventListener( 'click', e => {
@@ -87,59 +91,65 @@ import( './dom-ready' ).then( ( { default: domReady } ) => {
 			} );
 		}
 
-		const modals = [ ...( $$( '.search-modal, .mobile-menu-offset' ) ) ];
+		const modals = [ ...( $$( '.vite-modal' ) ) ];
 		if ( modals.length ) {
 			import( './handle-modal' ).then( ( { default: handleModal } ) => {
 				// Handle search toggle and modal.
-				handleModal( {
-					modalEl: $( '.search-modal' ),
-					openModalEl: $$( '.search-modal-open' ),
-					closeModalEl: $( '.search-modal-close' ),
-					onOpen: ( { modalEl } ) => {
-						const timeout = setTimeout( () => {
-							clearTimeout( timeout );
-							modalEl.querySelector( '.search-field' )?.focus();
-							import( './loop-focus' ).then( res => res.default( modalEl ) );
-						}, 100 );
-					},
-					onClose: ( { openModalEl } ) => {
-						if ( openModalEl.length ) {
-							openModalEl?.[ 1 ]?.focus();
-						} else {
-							openModalEl?.focus();
-						}
-					},
-				} );
-
-				// Handle mobile menu toggle and modal.
-				handleModal( {
-					modalEl: $( '.mobile-menu-offset' ),
-					openModalEl: $( '.mobile-menu-offset-open' ),
-					closeModalEl: $( '.mobile-menu-offset-close' ),
-					onOpen: ( { modalEl, closeModalEl } ) => {
-						const timeout = setTimeout( () => {
-							clearTimeout( timeout );
-							closeModalEl?.focus();
-							import( './loop-focus' ).then( res => res.default( modalEl ) );
-						}, 100 );
-						$( '.mobile-menu-offset' )?.addEventListener( 'click', e => {
-							if (
-								e.target === modalEl ||
-								( e.target?.closest( '.menu' ) && ! e.target?.closest( '.vite-sub-menu-toggle' ) )
-							) {
-								closeModalEl?.click();
+				const searchModal = $( '.vite-modal--search' );
+				if ( searchModal ) {
+					handleModal( {
+						modalEl: searchModal,
+						openModalEl: $$( '.vite-search__btn' ),
+						closeModalEl: searchModal.querySelector( '.vite-modal__btn' ),
+						onOpen: ( { modalEl } ) => {
+							const timeout = setTimeout( () => {
+								clearTimeout( timeout );
+								modalEl.querySelector( '.vite-search-form__input' )?.focus();
+								import( './loop-focus' ).then( res => res.default( modalEl ) );
+							}, 200 );
+						},
+						onClose: ( { openModalEl } ) => {
+							if ( openModalEl.length ) {
+								openModalEl?.[ 1 ]?.focus();
+							} else {
+								openModalEl?.focus();
 							}
-						} );
-					},
-					onClose: ( { openModalEl } ) => openModalEl?.focus(),
-				} );
+						},
+					} );
+				}
+				// Handle mobile menu toggle and modal.
+				const mobileMenuModal = $( '.vite-modal--mobile-menu' );
+				if ( mobileMenuModal ) {
+					handleModal( {
+						modalEl: mobileMenuModal,
+						openModalEl: $( '.vite-mobile-menu__btn' ),
+						closeModalEl: mobileMenuModal.querySelector( '.vite-modal__btn' ),
+						onOpen: ( { modalEl, closeModalEl } ) => {
+							const timeout = setTimeout( () => {
+								clearTimeout( timeout );
+								closeModalEl?.focus();
+								import( './loop-focus' ).then( res => res.default( modalEl ) );
+							}, 100 );
+							$( '.vite-modal--mobile-menu' )?.addEventListener( 'click', e => {
+								if (
+									e.target === modalEl ||
+									( e.target?.closest( '.menu' ) && ! e.target?.closest( '.vite-nav__submenu-toggle' ) )
+								) {
+									closeModalEl?.click();
+								}
+							} );
+						},
+						onClose: ( { openModalEl } ) => openModalEl?.focus(),
+					} );
+				}
 			} );
 		}
 
-		const masonry = $( '.vite-posts[data-masonry]' );
+		// Handle posts masonry layout.
+		const masonry = $( '.vite-posts--masonry' );
 		if ( masonry ) {
 			import( 'masonry-layout' ).then( ( { default: Masonry } ) => {
-				new Masonry( '.vite-posts[data-masonry]', {
+				new Masonry( masonry, {
 					itemSelector: '.vite-post',
 					columnWidth: '.vite-post',
 				} );
