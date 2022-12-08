07249c4467da0b64e47b2a857e5a54ec214cf5e2
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-12-08 17:51:32 +0545

    Add various scripts

--- inc/Vite.php
+++ inc/Vite.php
@@ -29,7 +29,7 @@ class Vite {
 	 */
 	public function __construct() {
 		$this->init();
-		vite( 'core' )->action( 'init' );
+		vite( 'core' )->action( 'loaded' );
 	}
 
 	/**
@@ -40,9 +40,9 @@ class Vite {
 	private function init() {
 		$this->init_hooks();
 		$this->init_aliases();
-		Supports::init();
-		TemplateHooks::init();
-		ScriptsStyles::init();
+
+		( new TemplateHooks() )->init();
+		( new ScriptsStyles() )->init();
 	}
 
 	/**
@@ -51,8 +51,7 @@ class Vite {
 	 * @return void
 	 */
 	private function init_aliases() {
-		$aliases = vite( 'core' )->filter( 'aliases', static::ALIASES );
-		foreach ( $aliases as $alias ) {
+		foreach ( static::ALIASES as $alias ) {
 			vite( $alias )->init();
 		}
 	}
@@ -63,7 +62,79 @@ class Vite {
 	 * @return void
 	 */
 	private function init_hooks() {
-		add_action( 'after_setup_theme', [ $this, 'load_textdomain' ] );
+		add_action( 'after_setup_theme', [ $this, 'setup' ] );
+	}
+
+	/**
+	 * Theme setup.
+	 *
+	 * @return void
+	 */
+	public function setup() {
+		$this->load_textdomain();
+		$this->add_theme_supports();
+		$this->add_image_sizes();
+	}
+
+	/**
+	 * Add theme supports.
+	 *
+	 * @return void
+	 */
+	private function add_theme_supports() {
+		$features = [
+			'html5'                               => [
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+			],
+			'post-thumbnails'                     => true,
+			'align-wide'                          => true,
+			'wp-block-styles'                     => true,
+			'editor-styles'                       => true,
+			'custom-logo'                         => vite( 'core' )->filter(
+				'custom-logo/args',
+				[
+					'width'       => 150,
+					'height'      => 75,
+					'flex-width'  => true,
+					'flex-height' => true,
+				]
+			),
+			'customize-selective-refresh-widgets' => true,
+			'title-tag'                           => true,
+			'automatic-feed-links'                => true,
+		];
+
+		/**
+		 * Filter: vite/supports.
+		 *
+		 * Filter the theme supports.
+		 *
+		 * @since x.x.x
+		 * @param array $features Theme supports.
+		 */
+		$features = vite( 'core' )->filter( 'supports', $features );
+
+		foreach ( $features as $feature => $args ) {
+			if ( $args && is_array( $args ) ) {
+				add_theme_support( $feature, $args );
+			} else {
+				add_theme_support( $feature );
+			}
+		}
+	}
+
+	/**
+	 * Add image sizes.
+	 *
+	 * @return void
+	 */
+	private function add_image_sizes() {
+		add_image_size( 'vite-featured-image-large', 1584, 992, true );
+		add_image_size( 'vite-featured-image', 540, 340, true );
 	}
 
 	/**
@@ -71,7 +142,7 @@ class Vite {
 	 *
 	 * @return void
 	 */
-	public function load_textdomain() {
+	private function load_textdomain() {
 		load_theme_textdomain( 'vite', get_template_directory() . '/languages' );
 	}
 }
