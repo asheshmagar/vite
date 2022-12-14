af4277c309f49b57687c51a946eddb2691a2dbfc
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-12-14 12:06:20 +0545

    Fix element not rendering

--- template-parts/footer/footer.php
+++ template-parts/footer/footer.php
@@ -8,47 +8,52 @@
 
 defined( 'ABSPATH' ) || exit;
 
-$configs = vite( 'core' )->get_theme_mod( 'footer' );
 ?>
 <footer id="colophon" class="vite-footer"<?php vite( 'seo' )->print_schema_microdata( 'footer' ); ?>>
 	<?php
+	$configs = vite( 'core' )->get_theme_mod( 'footer' )['desktop'];
+
 	foreach ( $configs as $row => $cols ) {
-		$is_row_empty = array_reduce(
-			array_values( $cols ),
-			function( $acc, $curr ) {
-				return empty( $curr );
-			},
-			false
+		$cols_with_content = array_filter(
+			$cols,
+			function( $col ) {
+				return ! empty( $col );
+			}
 		);
-		if ( $is_row_empty || empty( $cols ) ) {
+
+		if ( empty( $cols_with_content ) ) {
 			continue;
 		}
-		$classes = [
+
+		$col_keys  = array_keys( $cols_with_content );
+		$col_count = count( $col_keys );
+		$classes   = [
 			'vite-footer__row',
 			"vite-footer__row--$row",
 		];
+
 		$classes = vite( 'core' )->filter( 'footer/row/classes', $classes, $row );
 		?>
 		<div class="<?php echo esc_attr( implode( ' ', array_unique( $classes ) ) ); ?>">
 			<div class="vite-container">
 			<?php foreach ( $cols as $col => $elements ) : ?>
-					<div class="vite-header__col vite-header__col--<?php echo esc_attr( $col ); ?>">
-						<?php foreach ( $elements as $element ) : ?>
-<!--							<div class="vite-element vite-element----><?php //echo esc_attr( $element['id'] ); ?><!--">-->
-<!--								--><?php
-//									preg_match( '/\d+$/', $element['id'], $matches );
-//									$element_id   = preg_replace( '/(_|-)\d+/', '', $element['id'] );
-//									$element_type = $matches[0] ?? null;
-//									get_template_part(
-//										"template-parts/builder-components/$element_id",
-//										'',
-//										[
-//											'type'    => $element_type,
-//											'context' => 'footer',
-//										]
-//									);
-//								?>
-<!--							</div>-->
+					<div class="vite-footer__col vite-footer__col--<?php echo esc_attr( $col ); ?>">
+					<?php foreach ( $elements as $element ) : ?>
+							<div class="vite-element vite-element--<?php echo esc_attr( $element['id'] ); ?>">
+								<?php
+									preg_match( '/\d+$/', $element['id'], $matches );
+									$element_id   = preg_replace( '/(_|-)\d+/', '', $element['id'] );
+									$element_type = $matches[0] ?? null;
+									get_template_part(
+										"template-parts/builder-elements/$element_id",
+										'',
+										[
+											'type'    => $element_type,
+											'context' => 'footer',
+										]
+									);
+								?>
+							</div>
 						<?php endforeach; ?>
 					</div>
 				<?php endforeach; ?>
