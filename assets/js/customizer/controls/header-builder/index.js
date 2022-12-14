e0605fda309d70d5cf3922038b2d3b882b013962
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-12-14 12:35:14 +0545

    Combine builders

--- assets/js/customizer/controls/header-builder/index.js
+++
@@ -1,7 +0,0 @@
-import { registerControl } from '../../utils';
-import HeaderBuilder from './header-builder';
-import './customizer.scss';
-
-export default () => {
-	registerControl( 'vite-header-builder', HeaderBuilder );
-};
