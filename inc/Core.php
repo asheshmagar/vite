7dab182548cc4b86c981d15ca82cc0027a4beb0f
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-12-14 12:07:32 +0545

    Remove parse smart tags method

--- inc/Core.php
+++ inc/Core.php
@@ -96,30 +96,6 @@ class Core {
 	}
 
 	/**
-	 * Parse smart tags.
-	 *
-	 * @param string $content Content.
-	 * @return array|string|string[]
-	 */
-	public function parse_smart_tags( string $content = '' ) {
-		$smart_tags = [
-			'{{site-title}}' => get_bloginfo( 'name' ),
-			'{{site-url}}'   => home_url(),
-			'{{year}}'       => date_i18n( 'Y' ),
-			'{{date}}'       => gmdate( 'Y-m-d' ),
-			'{{time}}'       => gmdate( 'H:i:s' ),
-			'{{datetime}}'   => gmdate( 'Y-m-d H:i:s' ),
-			'{{copyright}}'  => '&copy;',
-		];
-
-		foreach ( $smart_tags as $tag => $value ) {
-			$content = str_replace( $tag, $value, $content );
-		}
-
-		return $content;
-	}
-
-	/**
 	 * Check if block editor is active.
 	 *
 	 * @return bool
