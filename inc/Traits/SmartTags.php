0509603d2a851b861117e96a7cbb29c97f931657
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-12-15 20:19:58 +0545

    Add theme-author smart tag

--- inc/Traits/SmartTags.php
+++ inc/Traits/SmartTags.php
@@ -12,13 +12,15 @@ trait SmartTags {
 	 */
 	public function parse_smart_tags( string $content = '' ) {
 		$smart_tags = [
-			'{{site-title}}' => get_bloginfo( 'name' ),
-			'{{site-url}}'   => home_url(),
-			'{{year}}'       => date_i18n( 'Y' ),
-			'{{date}}'       => gmdate( 'Y-m-d' ),
-			'{{time}}'       => gmdate( 'H:i:s' ),
-			'{{datetime}}'   => gmdate( 'Y-m-d H:i:s' ),
-			'{{copyright}}'  => '&copy;',
+			'{{site-title}}'   => get_bloginfo( 'name' ),
+			'{{site-url}}'     => home_url(),
+			'{{year}}'         => date_i18n( 'Y' ),
+			'{{date}}'         => gmdate( 'Y-m-d' ),
+			'{{time}}'         => gmdate( 'H:i:s' ),
+			'{{datetime}}'     => gmdate( 'Y-m-d H:i:s' ),
+			'{{copyright}}'    => 'Copyright &copy;',
+			/* Translators: %s: Theme author. */
+			'{{theme-author}}' => sprintf( __( 'Powered by %s' ), '<a href="https://wpvite.com" rel="nofollow noopener" target="_blank">Vite</a>' ),
 		];
 
 		foreach ( $smart_tags as $tag => $value ) {
