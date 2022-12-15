7f79018b6583ee8d83a23af01138670da1c86f84
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-12-15 20:23:29 +0545

    Allow responsive buttonset control

--- assets/js/customizer/controls/button-set/button-set.js
+++ assets/js/customizer/controls/button-set/button-set.js
@@ -1,6 +1,8 @@
 import { memo, useState, RawHTML } from '@wordpress/element';
 import { ButtonGroup, Button } from '@wordpress/components';
 import { isEqual } from 'lodash';
+import { useDeviceSelector } from '../../hooks';
+import { DEVICES } from '../../constants';
 
 export default memo( ( props ) => {
 	const {
@@ -13,12 +15,14 @@ export default memo( ( props ) => {
 				inputAttrs: {
 					multiple = false,
 					allow_reset: allowReset = true,
+					responsive = false,
 				},
 				default: defaultValue,
 			},
 		},
 	} = props;
 	const [ value, setValue ] = useState( setting.get() );
+	const { DeviceSelector, device } = useDeviceSelector();
 
 	return (
 		<div className="vite-control vite-button-set-control" data-multiple={ multiple }>
@@ -35,35 +39,60 @@ export default memo( ( props ) => {
 							className="vite-reset"
 						/>
 					) }
+					{ responsive && <DeviceSelector /> }
 				</div>
 			) }
 			<div className="vite-control-body">
-				<ButtonGroup>
-					{ Object.entries( choices ).map( ( [ key, val ] ) => (
-						<Button
-							key={ key }
-							onClick={ () => {
-								if ( multiple ) {
-									const temp = [ ...( value || [] ) ];
-									if ( temp.includes( key ) ) {
-										temp.splice( temp.indexOf( key ), 1 );
+				{ responsive ? (
+					DEVICES.map( ( d ) => (
+						device === d && (
+							<ButtonGroup key={ d }>
+								{ Object.keys( choices ).map( ( choice ) => (
+									<Button
+										key={ choice }
+										isPrimary={ value?.[ d ] === choice }
+										isSecondary={ value?.[ d ] !== choice }
+										onClick={ () => {
+											const temp = { ...value };
+											temp[ d ] = choice;
+											setValue( temp );
+											setting.set( temp );
+										} }
+									>
+										{ choices[ choice ] }
+									</Button>
+								) ) }
+							</ButtonGroup>
+						)
+					) )
+				) : (
+					<ButtonGroup>
+						{ Object.entries( choices ).map( ( [ key, val ] ) => (
+							<Button
+								key={ key }
+								onClick={ () => {
+									if ( multiple ) {
+										const temp = [ ...( value || [] ) ];
+										if ( temp.includes( key ) ) {
+											temp.splice( temp.indexOf( key ), 1 );
+										} else {
+											temp.push( key );
+										}
+										setValue( temp );
+										setting.set( temp );
 									} else {
-										temp.push( key );
+										setValue( key );
+										setting.set( key );
 									}
-									setValue( temp );
-									setting.set( temp );
-								} else {
-									setValue( key );
-									setting.set( key );
-								}
-							} }
-							variant={ multiple ? ( value?.includes( key ) ? 'primary' : 'secondary' ) : ( value === key ? 'primary' : 'secondary' ) }
-							className={ key }
-						>
-							{ val }
-						</Button>
-					) ) }
-				</ButtonGroup>
+								} }
+								variant={ multiple ? ( value?.includes( key ) ? 'primary' : 'secondary' ) : ( value === key ? 'primary' : 'secondary' ) }
+								className={ key }
+							>
+								{ val }
+							</Button>
+						) ) }
+					</ButtonGroup>
+				) }
 			</div>
 			{ description && (
 				<div className="customize-control-description">
