37d7d67f15ed9b7a4734b12ccfb2037f68fa5758
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-12-15 20:25:51 +0545

    Add buttons customizer settings

--- inc/Customizer/options/general/button.php
+++ inc/Customizer/options/general/button.php
@@ -6,6 +6,42 @@
  */
 
 $options = [
+	'vite[buttons-color]'         => [
+		'section'     => 'vite[global-buttons]',
+		'type'        => 'vite-color',
+		'title'       => __( 'Colors', 'vite' ),
+		'default'     => vite( 'core' )->get_theme_mod_defaults()['buttons-colors'],
+		'input_attrs' => [
+			'colors' => [
+				[
+					'id'    => '--button--color',
+					'label' => __( 'Normal', 'vite' ),
+				],
+				[
+					'id'    => '--button--hover--color',
+					'label' => __( 'Hover', 'vite' ),
+				],
+			],
+		],
+	],
+	'vite[buttons-bg-color]'      => [
+		'section'     => 'vite[global-buttons]',
+		'type'        => 'vite-color',
+		'title'       => __( 'Background Colors', 'vite' ),
+		'default'     => vite( 'core' )->get_theme_mod_defaults()['buttons-bg-colors'],
+		'input_attrs' => [
+			'colors' => [
+				[
+					'id'    => '--button--bg--color',
+					'label' => __( 'Normal', 'vite' ),
+				],
+				[
+					'id'    => '--button--hover--bg--color',
+					'label' => __( 'Hover', 'vite' ),
+				],
+			],
+		],
+	],
 	'vite[buttons-padding]'       => [
 		'section'     => 'vite[global-buttons]',
 		'type'        => 'vite-dimensions',
@@ -14,12 +50,14 @@ $options = [
 		'input_attrs' => [
 			'units'      => [ 'px', 'rem', 'em' ],
 			'responsive' => true,
+			'separator'  => true,
 		],
 	],
 	'vite[buttons-border]'        => [
 		'section'     => 'vite[global-buttons]',
 		'type'        => 'vite-border',
 		'title'       => __( 'Border', 'vite' ),
+		'default'     => vite( 'core' )->get_theme_mod_defaults()['buttons-border'],
 		'input_attrs' => [
 			'separator' => true,
 		],
@@ -28,10 +66,19 @@ $options = [
 		'section'     => 'vite[global-buttons]',
 		'type'        => 'vite-dimensions',
 		'title'       => __( 'Border Radius', 'vite' ),
+		'default'     => vite( 'core' )->get_theme_mod_defaults()['buttons-border-radius'],
 		'input_attrs' => [
 			'units'      => [ 'px', 'rem', 'em' ],
 			'responsive' => true,
 			'separator'  => true,
+		],
+	],
+	'vite[buttons-typography]'    => [
+		'section'     => 'vite[global-buttons]',
+		'type'        => 'vite-typography',
+		'title'       => __( 'Typography', 'vite' ),
+		'input_attrs' => [
+			'separator' => true,
 		],
 	],
 ];
