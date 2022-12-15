c217cf82a6aa439662caf2f823fae0d40810186f
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-12-15 20:26:14 +0545

    Add header html customizer settings

--- inc/Customizer/options/header/html.php
+++ inc/Customizer/options/header/html.php
@@ -27,6 +27,110 @@ $options = array_reduce(
 				},
 			],
 		];
+		$acc[ "vite[header-html-$curr-alignment]" ]  = [
+			'section'     => "vite[header-html-$curr]",
+			'type'        => 'vite-buttonset',
+			'title'       => __( 'Alignment', 'vite' ),
+			'default'     => vite( 'core' )->get_theme_mod_defaults()[ "header-html-$curr-alignment" ],
+			'partial'     => [
+				'selector'        => ".vite-header .vite-html--$curr",
+				'render_callback' => function() use ( $curr ) {
+					get_template_part(
+						'template-parts/builder-elements/html',
+						'',
+						[
+							'type'    => $curr,
+							'context' => 'header',
+						]
+					);
+				},
+			],
+			'choices'     => [
+				'left'   => __( 'Left', 'vite' ),
+				'center' => __( 'Center', 'vite' ),
+				'right'  => __( 'Right', 'vite' ),
+			],
+			'input_attrs' => [
+				'responsive' => true,
+				'separator'  => true,
+			],
+		];
+		$acc[ "vite[header-html-$curr-display]" ]    = [
+			'section'     => "vite[header-html-$curr]",
+			'type'        => 'vite-buttonset',
+			'title'       => __( 'Display on', 'vite' ),
+			'default'     => vite( 'core' )->get_theme_mod_defaults()[ "header-html-$curr-display" ],
+			'partial'     => [
+				'selector'        => ".vite-header .vite-html--$curr",
+				'render_callback' => function() use ( $curr ) {
+					get_template_part(
+						'template-parts/builder-elements/html',
+						'',
+						[
+							'type'    => $curr,
+							'context' => 'header',
+						]
+					);
+				},
+			],
+			'choices'     => [
+				'desktop' => __( 'Desktop', 'vite' ),
+				'tablet'  => __( 'Tablet', 'vite' ),
+				'mobile'  => __( 'Mobile', 'vite' ),
+			],
+			'input_attrs' => [
+				'multiple'  => true,
+				'separator' => true,
+			],
+		];
+		$acc[ "vite[header-html-$curr-text-color]" ] = [
+			'section'     => "vite[header-html-$curr]",
+			'type'        => 'vite-color',
+			'title'       => __( 'Text Color', 'vite' ),
+			'input_attrs' => [
+				'separator' => true,
+				'colors'    => [
+					[
+						'id'    => 'normal',
+						'label' => __( 'Normal', 'vite' ),
+					],
+				],
+			],
+		];
+		$acc[ "vite[header-html-$curr-link-color]" ] = [
+			'section'     => "vite[header-html-$curr]",
+			'type'        => 'vite-color',
+			'title'       => __( 'Link Colors', 'vite' ),
+			'input_attrs' => [
+				'colors'    => [
+					[
+						'id'    => 'normal',
+						'label' => __( 'Normal', 'vite' ),
+					],
+					[
+						'id'    => 'hover',
+						'label' => __( 'Hover', 'vite' ),
+					],
+				],
+			],
+		];
+		$acc[ "vite[header-html-$curr-typography]" ] = [
+			'section'     => "vite[header-html-$curr]",
+			'type'        => 'vite-typography',
+			'title'       => __( 'Typography', 'vite' ),
+			'input_attrs' => [
+				'separator' => true,
+			],
+		];
+		$acc[ "vite[header-html-$curr-margin]" ]     = [
+			'section'     => "vite[header-html-$curr]",
+			'type'        => 'vite-dimensions',
+			'title'       => __( 'Margin', 'vite' ),
+			'input_attrs' => [
+				'separator'  => true,
+				'responsive' => true,
+			],
+		];
 		return $acc;
 	},
 	[]
