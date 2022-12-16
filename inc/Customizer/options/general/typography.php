fe0f8460e4a150844f5b35c673d49e7ea8795648
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-12-16 19:35:29 +0545

    Use loop to add settings

--- inc/Customizer/options/general/typography.php
+++ inc/Customizer/options/general/typography.php
@@ -5,67 +5,35 @@
  * @package Vite
  */
 
-$options = [
-	'vite[base-typography]' => [
-		'section'   => 'vite[global-typography]',
-		'type'      => 'vite-typography',
-		'title'     => __( 'Base', 'vite' ),
-		'selectors' => [ 'body' ],
-	],
-	'vite[h1-typography]'   => [
-		'section'     => 'vite[global-typography]',
-		'type'        => 'vite-typography',
-		'title'       => __( 'Heading 1 (h1)', 'vite' ),
-		'selectors'   => [ 'body' ],
-		'input_attrs' => [
-			'separator' => true,
-		],
-	],
-	'vite[h2-typography]'   => [
-		'section'     => 'vite[global-typography]',
-		'type'        => 'vite-typography',
-		'title'       => __( 'Heading 2 (h2)', 'vite' ),
-		'selectors'   => [ 'body' ],
-		'input_attrs' => [
-			'separator' => true,
-		],
-	],
-	'vite[h3-typography]'   => [
-		'section'     => 'vite[global-typography]',
-		'type'        => 'vite-typography',
-		'title'       => __( 'Heading 3 (h3)', 'vite' ),
-		'selectors'   => [ 'body' ],
-		'input_attrs' => [
-			'separator' => true,
-		],
-	],
-	'vite[h4-typography]'   => [
-		'section'     => 'vite[global-typography]',
-		'type'        => 'vite-typography',
-		'title'       => __( 'Heading 4 (h4)', 'vite' ),
-		'selectors'   => [ 'body' ],
-		'input_attrs' => [
-			'separator' => true,
-		],
-	],
-	'vite[h5-typography]'   => [
-		'section'     => 'vite[global-typography]',
-		'type'        => 'vite-typography',
-		'title'       => __( 'Heading 5 (h5)', 'vite' ),
-		'selectors'   => [ 'body' ],
-		'input_attrs' => [
-			'separator' => true,
-		],
-	],
-	'vite[h6-typography]'   => [
-		'section'     => 'vite[global-typography]',
-		'type'        => 'vite-typography',
-		'title'       => __( 'Heading 6 (h6)', 'vite' ),
-		'selectors'   => [ 'body' ],
-		'input_attrs' => [
-			'separator' => true,
-		],
-	],
+$elements = [
+	'base'     => __( 'Base', 'vite' ),
+	'headings' => __( 'Headings', 'vite' ),
+	'h1'       => __( 'H1', 'vite' ),
+	'h2'       => __( 'H2', 'vite' ),
+	'h3'       => __( 'H3', 'vite' ),
+	'h4'       => __( 'H4', 'vite' ),
+	'h5'       => __( 'H5', 'vite' ),
+	'h6'       => __( 'H6', 'vite' ),
 ];
+
+$options = array_reduce(
+	array_keys( $elements ),
+	function( $acc, $curr ) use ( $elements ) {
+		$acc[ "vite[$curr-typography]" ] = [
+			'section'   => 'vite[global-typography]',
+			'type'      => 'vite-typography',
+			'title'     => $elements[ $curr ],
+			'selectors' => [ 'body' ],
+		];
+
+		if ( 'base' !== $curr ) {
+			$acc[ "vite[$curr-typography]" ]['input_attrs'] = [
+				'separator' => true,
+			];
+		}
+		return $acc;
+	},
+	[]
+);
 
 vite( 'customizer' )->add( 'settings', $options );
