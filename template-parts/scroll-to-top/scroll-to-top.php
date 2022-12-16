f34a8ed13f7cbfde335c2bcc879a5d1085aef84f
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-12-16 19:31:39 +0545

    Add template for scroll to top

---
+++ template-parts/scroll-to-top/scroll-to-top.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template part for displaying scroll to top button.
+ *
+ * @package Vite
+ */
+
+$core          = vite( 'core' );
+$scroll_to_top = $core->get_theme_mod( 'scroll-to-top', true );
+
+if ( ! $scroll_to_top ) {
+	return;
+}
+
+$position  = $core->get_theme_mod( 'scroll-to-top-position', 'right' );
+$icon      = $core->get_theme_mod( 'scroll-to-top-icon', 'arrow-up' );
+$icon_size = $core->get_theme_mod( 'scroll-to-top-icon-size', 13 );
+?>
+<div class="vite-modal vite-modal--stt vite-modal--stt--pos-<?php echo esc_attr( $position ); ?>">
+	<button aria-label="<?php esc_html_e( 'Scroll to top', 'vite' ); ?>">
+		<?php
+		vite( 'icon' )->get_icon(
+			$icon,
+			[
+				'echo' => true,
+				'size' => (int) $icon_size,
+			]
+		);
+		?>
+	</button>
+</div>
