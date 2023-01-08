65b9e676b04dd8b750268798553abba850cc3f29
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2023-01-08 18:32:53 +0545

    Remove reset export

--- assets/js/customizer/components/index.js
+++ assets/js/customizer/components/index.js
@@ -2,7 +2,6 @@ export { default as Tooltip } from './tooltip';
 export { default as ViteColorPicker } from './color-picker';
 export { default as ViteRange } from './range';
 export { default as Popover } from './popover';
-export { default as Reset } from './reset';
 export { default as ViteDimensions } from './dimensions';
 export { default as RawHTML } from './raw-html';
 export { default as Icon } from './icon';
