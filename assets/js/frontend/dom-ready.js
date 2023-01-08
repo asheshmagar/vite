143a95ed84518b53ae94e29ed4b14941296094f6
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2023-01-08 18:35:53 +0545

    Merge small js scripts to one

--- assets/js/frontend/dom-ready.js
+++
@@ -1,7 +0,0 @@
-export default ( callback ) => {
-	if ( document.readyState === 'complete' || document.readyState === 'interactive' ) {
-		callback();
-	} else {
-		document.addEventListener( 'DOMContentLoaded', callback );
-	}
-};
