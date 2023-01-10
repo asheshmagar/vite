eedaf5bb0e84c6aff4051a78ddabb858e1a14915
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2023-01-10 16:28:04 +0545

    Add compat function that might not be available

---
+++ inc/Compat.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * PHP functions either missing from older PHP versions or not included by default.
+ *
+ * @package Vite
+ */
+
+if ( ! function_exists( 'str_starts_with' ) ) {
+
+	/**
+	 * Polyfill for `str_starts_with()` function added in PHP 8.0.
+	 *
+	 * Performs a case-sensitive check indicating if
+	 * the haystack begins with needle.
+	 *
+	 * @param string $haystack The string to search in.
+	 * @param string $needle   The substring to search for in the `$haystack`.
+	 *
+	 * @return bool True if `$haystack` starts with `$needle`, otherwise false.
+	 */
+	function str_starts_with( string $haystack, string $needle ): bool {
+		return '' === $needle || 0 === strpos( $haystack, $needle );
+	}
+}
+
+if ( ! function_exists( 'str_ends_with' ) ) {
+
+	/**
+	 * Polyfill for `str_ends_with()` function added in PHP 8.0.
+	 *
+	 * Performs a case-sensitive check indicating if
+	 * the haystack ends with needle.
+	 *
+	 * @param string $haystack The string to search in.
+	 * @param string $needle   The substring to search for in the `$haystack`.
+	 *
+	 * @return bool True if `$haystack` ends with `$needle`, otherwise false.
+	 */
+	function str_ends_with( string $haystack, string $needle ): bool {
+		return '' === $needle || substr( $haystack, -strlen( $needle ) ) === $needle;
+	}
+}
+
+if ( ! function_exists( 'str_contains' ) ) {
+
+	/**
+	 * Polyfill for `str_contains()` function added in PHP 8.0.
+	 *
+	 * Performs a case-sensitive check indicating if needle is
+	 * contained in haystack.
+	 *
+	 * @param string $haystack The string to search in.
+	 * @param string $needle   The substring to search for in the haystack.
+	 *
+	 * @return bool True if `$needle` is in `$haystack`, otherwise false.
+	 */
+	function str_contains( string $haystack, string $needle ): bool {
+		return '' === $needle || false !== strpos( $haystack, $needle );
+	}
+}
+
