edb05b5b55987a82c71a7ad60bea5f45e5f98946
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2023-01-10 16:28:19 +0545

    Add new search icon

--- inc/Icon.php
+++ inc/Icon.php
@@ -23,7 +23,8 @@ class Icon {
 	 * Icon constructor.
 	 */
 	public function __construct() {
-		$this->icons = $this->json_to_array( VITE_ASSETS_DIR . '/json/font-awesome.json' );
+		$this->icons                = $this->json_to_array( VITE_ASSETS_DIR . '/json/font-awesome.json' );
+		$this->icons['vite-search'] = "<svg xmlns='http://www.w3.org/2000/svg' class='search %s' width='%d' height='%d' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><circle cx='11' cy='11' r='8'></circle><line x1='21' y1='21' x2='16.65' y2='16.65'></line></svg>";
 	}
 
 	/**
@@ -66,14 +67,19 @@ class Icon {
 			'svg/allowed-html',
 			[
 				'svg'     => [
-					'class'       => true,
-					'xmlns'       => true,
-					'width'       => true,
-					'height'      => true,
-					'viewbox'     => true,
-					'aria-hidden' => true,
-					'role'        => true,
-					'focusable'   => true,
+					'class'           => true,
+					'xmlns'           => true,
+					'width'           => true,
+					'height'          => true,
+					'viewbox'         => true,
+					'aria-hidden'     => true,
+					'role'            => true,
+					'focusable'       => true,
+					'fill'            => true,
+					'stroke'          => true,
+					'stroke-width'    => true,
+					'stroke-linecap'  => true,
+					'stroke-linejoin' => true,
 				],
 				'path'    => [
 					'fill'      => true,
@@ -92,6 +98,12 @@ class Icon {
 					'points'    => true,
 					'transform' => true,
 					'focusable' => true,
+				],
+				'line'    => [
+					'x1' => true,
+					'y1' => true,
+					'x2' => true,
+					'y2' => true,
 				],
 			]
 		);
