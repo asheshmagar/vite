fd0f6cf8c3b27ce7f62134c577903406c4defdb3
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2023-01-18 18:56:53 +0545

    Refactor clean up

--- functions.php
+++ functions.php
@@ -9,22 +9,13 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-/**
- * Define constants.
- */
-! defined( 'VITE_VERSION' ) && define( 'VITE_VERSION', '1.0.0' );
-! defined( 'VITE_PHP_MIN_VERSION' ) && define( 'VITE_PHP_MIN_VERSION', '7.2' );
-! defined( 'VITE_WP_MIN_VERSION' ) && define( 'VITE_WP_MIN_VERSION', '5.5' );
-! defined( 'VITE_ASSETS_DIR' ) && define( 'VITE_ASSETS_DIR', get_theme_file_path( '/assets/' ) );
-! defined( 'VITE_ASSETS_URI' ) && define( 'VITE_ASSETS_URI', get_theme_file_uri( '/assets/' ) );
-
-require_once __DIR__ . '/inc/BackCompat.php';
+require_once __DIR__ . '/inc/Constants.php'; // Load theme constants.
+require_once __DIR__ . '/inc/BackCompat.php'; // Load theme back-compat.
 
 if ( ! call_user_func( 'Vite\maybe_load_theme' ) ) {
 	return;
 }
 
-// Load composer autoloader.
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php'; // Load composer autoloader.
 
-vite( 'theme' )->init();
+vite( 'theme' )->init(); // Init theme.
