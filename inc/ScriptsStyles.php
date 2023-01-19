7b0af80af07041dfb6075431854c621b895745b7
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2023-01-19 10:16:57 +0545

    Minor reformat

--- inc/ScriptsStyles.php
+++ inc/ScriptsStyles.php
@@ -85,8 +85,8 @@ class ScriptsStyles {
 	public function enqueue() {
 		$remote_google_fonts_url = $this->get_theme_mod( 'google-fonts-url', '' );
 		$is_local_google_fonts   = $this->get_theme_mod( 'local-google-fonts', false );
-		$dynamic_css             = vite( 'customizer' )->dynamic_css->get();
 		$dynamic_css_output      = $this->get_theme_mod( 'dynamic-css-output', 'inline' );
+		$dynamic_css             = vite( 'customizer' )->dynamic_css->get();
 
 		if ( ! empty( $remote_google_fonts_url ) ) {
 			if ( $is_local_google_fonts ) {
