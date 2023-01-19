81721c751596f9a0a207d663e9a6db3d7ddec379
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2023-01-19 10:23:38 +0545

    Change description

--- inc/Customizer/options/footer/html.php
+++ inc/Customizer/options/footer/html.php
@@ -8,12 +8,12 @@
 $options = array_reduce(
 	[ 1, 2 ],
 	function( $acc, $curr ) {
-		$acc[ "vite[footer-html-$curr]" ]            = [
-			'section' => "vite[footer-html-$curr]",
-			'type'    => 'vite-editor',
-			'title'   => __( 'HTML', 'vite' ),
-			'default' => vite( 'core' )->get_theme_mod_defaults()[ "footer-html-$curr" ],
-			'partial' => [
+		$acc[ "vite[footer-html-$curr]" ] = [
+			'section'     => "vite[footer-html-$curr]",
+			'type'        => 'vite-editor',
+			'title'       => __( 'HTML', 'vite' ),
+			'default'     => vite( 'core' )->get_theme_mod_defaults()[ "footer-html-$curr" ],
+			'partial'     => [
 				'selector'        => ".vite-footer .vite-html--$curr",
 				'render_callback' => function() use ( $curr ) {
 					get_template_part(
@@ -26,6 +26,11 @@ $options = array_reduce(
 					);
 				},
 			],
+			'description' => sprintf(
+				/* Translators: %s: Smart tags description */
+				__( 'Shortcodes and HTML tags are allowed. %s', 'vite' ),
+				vite( 'core' )->description_smart_tags()
+			),
 		];
 		$acc[ "vite[footer-html-$curr-alignment]" ]  = [
 			'section'     => "vite[footer-html-$curr]",
