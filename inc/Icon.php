dcfbb76760b24bfd454cdd12bab0c60b177f076c
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2023-01-19 19:03:59 +0545

    Remove icons property
    Dynamically register icon property

--- inc/Icon.php
+++ inc/Icon.php
@@ -14,17 +14,10 @@ class Icon {
 	use Hook, JSON;
 
 	/**
-	 * Icons list.
-	 *
-	 * @var mixed $icon
-	 */
-	public $icons;
-
-	/**
 	 * Icon constructor.
 	 */
 	public function __construct() {
-		$this->icons                = $this->json_to_array( VITE_ASSETS_DIR . '/json/font-awesome.json' );
+		$this->json_to_array( VITE_ASSETS_DIR . '/json/font-awesome.json', 'icons' );
 		$this->icons['vite-search'] = "<svg xmlns='http://www.w3.org/2000/svg' class='search %s' width='%d' height='%d' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><circle cx='11' cy='11' r='8'></circle><line x1='21' y1='21' x2='16.65' y2='16.65'></line></svg>";
 	}
 
