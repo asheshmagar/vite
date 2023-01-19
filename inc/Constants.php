b88cd52b0a445d38a32e624fd1e3d61f607a304f
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2023-01-19 10:58:26 +0545

    Change PHP min version constant value

--- inc/Constants.php
+++ inc/Constants.php
@@ -8,7 +8,7 @@
 defined( 'ABSPATH' ) || exit;
 
 ! defined( 'VITE_VERSION' ) && define( 'VITE_VERSION', '1.0.0' );
-! defined( 'VITE_PHP_MIN_VERSION' ) && define( 'VITE_PHP_MIN_VERSION', '7.2' );
+! defined( 'VITE_PHP_MIN_VERSION' ) && define( 'VITE_PHP_MIN_VERSION', '7.0' );
 ! defined( 'VITE_WP_MIN_VERSION' ) && define( 'VITE_WP_MIN_VERSION', '5.5' );
 ! defined( 'VITE_ASSETS_DIR' ) && define( 'VITE_ASSETS_DIR', get_theme_file_path( '/assets/' ) );
 ! defined( 'VITE_ASSETS_URI' ) && define( 'VITE_ASSETS_URI', get_theme_file_uri( '/assets/' ) );
