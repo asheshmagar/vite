aedd6157ff6f178056e183fb33ca3756dcbfa6df
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2023-01-24 18:05:09 +0545

    Setup bbPress plugin compat class

---
+++ inc/Compatibility/BBPress.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Vite\Compatibility;
+
+defined( 'ABSPATH' ) || exit;
+
+class BBPress extends AbstractCompatibility {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function init() {
+		// TODO: Implement init() method.
+	}
+}
