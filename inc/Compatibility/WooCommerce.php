ca23220f84f52d9593c475aea6ebf3a9d5414660
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2023-01-24 18:06:06 +0545

    Setup wc plugin compat class

---
+++ inc/Compatibility/WooCommerce.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * WooCommerce's compatibility.
+ *
+ * @package Vite
+ */
+
+namespace Vite\Compatibility;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * WooCommerce's compatibility.
+ */
+class WooCommerce extends AbstractCompatibility {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function init() {
+		add_action( 'after_setup_theme', [ $this, 'setup' ] );
+		add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+		add_filter( 'woocommerce_show_page_title', '__return_false' );
+	}
+
+	/**
+	 * Setup WooCommerce.
+	 *
+	 * @return void
+	 */
+	public function setup() {
+		add_theme_support( 'woocommerce' );
+
+		$features = $this->filter(
+			'wc/features',
+			[
+				'wc-product-gallery-zoom',
+				'wc-product-gallery-lightbox',
+				'wc-product-gallery-slider',
+			]
+		);
+
+		foreach ( $features as $feature ) {
+			add_theme_support( $feature );
+		}
+	}
+}
