b50b6e8ca37e0da5a064152ed5194ec7e86067f5
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2023-01-24 18:05:31 +0545

    Setup lifterLMS plugin compat class

---
+++ inc/Compatibility/LifterLMS.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Vite\Compatibility;
+
+defined( 'ABSPATH' ) || exit;
+
+class LifterLMS extends AbstractCompatibility {
+
+	public function init() {
+		// TODO: Implement init() method.
+	}
+}
