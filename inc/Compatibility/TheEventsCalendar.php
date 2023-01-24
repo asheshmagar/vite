a89f816cfd3888d24f3dd053e070610192f86366
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2023-01-24 18:05:47 +0545

    Setup events calendar plugin compat class

---
+++ inc/Compatibility/TheEventsCalendar.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Vite\Compatibility;
+
+defined( 'ABSPATH' ) || exit;
+
+class TheEventsCalendar extends AbstractCompatibility {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function init() {
+		// TODO: Implement init() method.
+	}
+}
