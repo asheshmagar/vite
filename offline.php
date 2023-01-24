88f840fb2766e51f1402f59d0b083607872e2182
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2023-01-24 17:54:49 +0545

    Add offline PWA template

---
+++ offline.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for offline page in PWA.
+ *
+ * @package Vite
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+/**
+ * Action: vite/pwa/offline/start.
+ */
+vite( 'core' )->action( 'pwa/offline/start' );
+
+/**
+ * Action: vite/pwa/offline.
+ *
+ * @see \Vite\Compatibility\PWA::offline()
+ */
+vite( 'core' )->action( 'pwa/offline' );
+
+/**
+ * Action: vite/pwa/offline/end.
+ */
+vite( 'core' )->action( 'pwa/offline/end' );
+?>
+	</div>
+<?php
+get_footer();
+
+
