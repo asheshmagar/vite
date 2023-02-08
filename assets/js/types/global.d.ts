56afe67a913027279e2736e6f9a0cd420877f288
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2023-02-08 18:15:54 +0545

    More decret global types properties

--- assets/js/types/global.d.ts
+++ assets/js/types/global.d.ts
@@ -6,6 +6,39 @@ declare global {
 
 declare var wp: any;
 declare var tinymce: any;
-declare var _VITE_: any;
-declare var _VITE_CUSTOMIZER_PREVIEW_: any;
-declare var _VITE_CUSTOMIZER_: any;
+declare var _VITE_: {
+	initMasonryInfiniteScroll: () => void;
+	initNavigation: () => void;
+	initModals: () => void;
+	initScrollToTop: () => void;
+	init: () => void;
+	publicPath: string;
+};
+declare var _VITE_CUSTOMIZER_PREVIEW_: {
+	configs: {
+		[key: string]: any
+	}[];
+};
+declare var _VITE_CUSTOMIZER_: {
+	icons: {
+		[key: string]: string
+	}[];
+	condition: {
+		[key:string]: {
+			[key:string]: any
+		}
+	};
+	conditions: {
+		[key: string]: {
+			relation?: 'AND' | 'OR';
+			terms?: {
+				name: string;
+				operator: '===' | '!==' | '==' | '!=' | '>' | '>=' | '<' | '<=' | 'contains' | '!contains' | 'in' | '!in';
+				value: any;
+			};
+		}
+	};
+	publicPath: string;
+	resetNonce: string;
+	ajaxURL: string;
+};
