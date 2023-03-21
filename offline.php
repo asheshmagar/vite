cf952cfc88320bc9d6b930a7a0ddbe1a87394861
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2023-03-21 15:59:52 +0545

    Store instance of core class in variable

--- offline.php
+++ offline.php
@@ -7,24 +7,26 @@
 
 defined( 'ABSPATH' ) || exit;
 
+$core = vite( 'core' );
+
 get_header();
 
 /**
  * Action: vite/pwa/offline/start.
  */
-vite( 'core' )->action( 'pwa/offline/start' );
+$core->action( 'pwa/offline/start' );
 
 /**
  * Action: vite/pwa/offline.
  *
  * @see \Vite\Compatibility\Plugin\PWA::offline()
  */
-vite( 'core' )->action( 'pwa/offline' );
+$core->action( 'pwa/offline' );
 
 /**
  * Action: vite/pwa/offline/end.
  */
-vite( 'core' )->action( 'pwa/offline/end' );
+$core->action( 'pwa/offline/end' );
 ?>
 	</div>
 <?php
